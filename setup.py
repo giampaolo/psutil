91ef217acc203761468544dc41cbdcc5e751fac1
Jay Loden <jloden@gmail.com>
2008-05-05 16:56:06 +0000

    Moved setup script to main directory and added package data so the py files can be installed in site-packages

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+from distutils.core import setup, Extension 
+
+# Windows
+if sys.platform.lower().startswith("win"):
+    # build Windows module
+    module1 = Extension('psutil/_psutil_mswindows', sources = ['psutil/_psutil_mswindows.c'],
+                        define_macros=[('_WIN32_WINNT', '0x0500')],
+                        libraries=["psapi"])
+
+    setup (name = 'PsutilMswindows',
+            version = '1.0',
+            description = 'Windows implementation',
+            ext_modules = [module1])
+
+
+# OS X
+if sys.platform.lower().startswith("darwin"): 
+    # build OS X module
+    module2 = Extension('psutil/_psutil_osx', sources = ['psutil/_psutil_osx.c'],)
+    setup (name = 'PsutilOSX',
+            version = '1.0',
+            description = 'OS X implementation',
+            ext_modules = [module2])
+
+if sys.platform.lower().startswith("linux"):
+    # build Linux module
+    # TODO: no compilation needed here
+    pass
+
+
+setup(name='psutil',
+      version='1.0',
+      description='Python Process Management Library',
+      author='Giampaolo Rodola, Dave Daeschler, Jay Loden',
+      author_email='psutil-dev@googlegroups.com',
+      url='http://code.google.com/p/psutil/',
+      packages=['psutil'],
+     )
