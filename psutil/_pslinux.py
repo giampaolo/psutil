96e39e12575e65da08854740607d8c7e9d76e381
Giampaolo Rodola' <g.rodola@gmail.com>
2009-01-27 14:26:58 +0000

    Fix Issue #5 (can't determine name/path for low pid processes)

--- psutil/_pslinux.py
+++ psutil/_pslinux.py
@@ -15,17 +15,32 @@ class Impl(object):
         """Returns a process info class."""
         # XXX - figure out why it can't be imported globally (see r54)
         import psutil
+
+        # determine executable
         try:
-            exe = os.readlink("/proc/%s/exe" %pid)
+            _exe = os.readlink("/proc/%s/exe" %pid)
         except OSError:
-            exe = '<unknown>'
-        path, name = os.path.split(exe)
+            f = open("/proc/%s/stat" %pid)
+            try:
+                _exe = f.read().split(' ')[1].replace('(', '').replace(')', '')
+            finally:
+                f.close()
+
+        # determine name and path
+        if os.path.isabs(_exe):
+            path, name = os.path.split(_exe)
+        else:
+            path = '<unknown>'
+            name = _exe
+
+        # determine cmdline
         f = open("/proc/%s/cmdline" %pid)
         try:
             # return the args as a list, dropping the last line (always empty)
             cmdline = f.read().split('\x00')[:-1]
         finally:
             f.close()
+
         return psutil.ProcessInfo(pid, name, path, cmdline)
 
     def kill_process(self, pid, sig=signal.SIGKILL):
