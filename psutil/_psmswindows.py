4aa17a790f5771a7a176baa3220cbe1a0dc273cd
Jay Loden <jloden@gmail.com>
2009-03-03 15:36:27 +0000

    Implemented dummy get_username and get_group for Windows platform until we can implement username/groupname properties for Windows.

--- psutil/_psmswindows.py
+++ psutil/_psmswindows.py
@@ -51,6 +51,14 @@ class Impl(object):
                 raise psutil.AccessDenied
             raise
 
+    @wrap_privileges
+    def get_username(self, uid):
+        return ""
+
+    @wrap_privileges
+    def get_group(self, gid):
+        return ""
+
     def get_pid_list(self):
         """Returns a list of PIDs currently running on the system."""
         return _psutil_mswindows.get_pid_list()
