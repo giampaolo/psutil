38a9a8de6ccdefdd44cd83e9745aa2c22578e388
Giampaolo Rodola' <g.rodola@gmail.com>
2009-03-24 17:14:56 +0000

    Created a test_* method for every Process function we are testing so that if we get a failure unittest will continue to test other functions.

--- test/test_memory_leaks.py
+++ test/test_memory_leaks.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# test_memory_leaks.oy
+# test_memory_leaks.py
 
 import os
 import gc
@@ -10,49 +10,68 @@ import psutil
 LOOPS = 250
 
 
-class TestCase(unittest.TestCase):
+class TestMemoryLeaks(unittest.TestCase):
 
-    def test_memory_leak(self):
+    def setUp(self):
+        gc.collect()
 
-        def execute(method):
-            # step 1
-            for x in xrange(LOOPS):
-                p = psutil.Process(os.getpid())
-                obj = getattr(p, method)
-                if callable(obj):
-                    retvalue = obj()
-                else:
-                    retvalue = obj  # property
-            del x, p, obj, retvalue
-            gc.collect()
-            rss1 = psutil.Process(os.getpid()).get_memory_info()[0]
+    def execute(self, method, *args, **kwarks):
+        # step 1
+        for x in xrange(LOOPS):
+            p = psutil.Process(os.getpid())
+            obj = getattr(p, method)
+            if callable(obj):
+                retvalue = obj(*args, **kwarks)
+            else:
+                retvalue = obj  # property
+        del x, p, obj, retvalue
+        gc.collect()
+        rss1 = psutil.Process(os.getpid()).get_memory_info()[0]
 
-            # step 2
-            for x in xrange(LOOPS):
-                p = psutil.Process(os.getpid())
-                obj = getattr(p, method)
-                if callable(obj):
-                    retvalue = obj()
-                else:
-                    retvalue = obj  # property
-            del x, p, obj, retvalue
-            gc.collect()
-            rss2 = psutil.Process(os.getpid()).get_memory_info()[0]
+        # step 2
+        for x in xrange(LOOPS):
+            p = psutil.Process(os.getpid())
+            obj = getattr(p, method)
+            if callable(obj):
+                retvalue = obj(*args, **kwarks)
+            else:
+                retvalue = obj  # property
+        del x, p, obj, retvalue
 
-            # comparison
-            self.assertEqual(rss1, rss2)
+        rss2 = psutil.Process(os.getpid()).get_memory_info()[0]
 
-        execute('create_time')
-        execute('get_cpu_times')
-        execute('get_cpu_percent')
-        execute('get_memory_info')
-        execute('get_memory_percent')
-        execute('__str__')
+        # comparison
+        self.assertEqual(rss1, rss2)
+
+    def test_pid(self):
+        self.execute('pid')
+
+    def test__str__(self):
+        # includes name, ppid, path, cmdline, uid, gid properties
+        self.execute('__str__')
+
+    def test_create_time(self):
+        self.execute('create_time')
+
+    def test_get_cpu_times(self):
+        self.execute('get_cpu_times')
+
+    def test_get_cpu_percent(self):
+        self.execute('get_cpu_percent')
+
+    def test_get_memory_info(self):
+        self.execute('get_memory_info')
+
+    def test_get_memory_percent(self):
+        self.execute('get_memory_percent')
+
+    def test_is_running(self):
+        self.execute('is_running')
 
 
 def test_main():
     test_suite = unittest.TestSuite()
-    test_suite.addTest(unittest.makeSuite(TestCase))
+    test_suite.addTest(unittest.makeSuite(TestMemoryLeaks))
     unittest.TextTestRunner(verbosity=2).run(test_suite)
 
 if __name__ == '__main__':
