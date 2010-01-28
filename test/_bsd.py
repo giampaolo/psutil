78bcc421a1271d3347aee303e44ac92ed3d63a27
Giampaolo Rodola' <g.rodola@gmail.com>
2010-01-28 21:22:55 +0000

    added TOTAL_PHYMEM test specific for BSD

--- test/_bsd.py
+++ test/_bsd.py
@@ -1,5 +1,15 @@
 import unittest
+import subprocess
+
+import psutil
+
 
 class BSDSpecificTestCase(unittest.TestCase):
-    pass
+
+    def test_TOTAL_PHYMEM(self):
+        p = subprocess.Popen('sysctl -a | grep hw.physmem', shell=1, stdout=subprocess.PIPE)
+        out = p.stdout.read()
+        value = int(out.split()[1])
+        self.assertEqual(value, psutil.TOTAL_PHYMEM)
+
 
