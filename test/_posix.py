2a2eaceaa47527c3bbb37039b2ac4aaef3420234
Giampaolo Rodola' <g.rodola@gmail.com>
2010-01-30 01:48:38 +0000

    Parsed the output returned by ps so that it works fine on freebsd. There are some -0 arguments which are not recognized on BSD. A workaround is necessary.

--- test/_posix.py
+++ test/_posix.py
@@ -3,6 +3,7 @@
 import unittest
 import subprocess
 import time
+import sys
 
 import psutil
 
@@ -11,8 +12,14 @@ from test_psutil import kill, PYTHON, DEVNULL
 
 def sh(cmd):
     """Execute a shell command and return its output."""
+    if not sys.platform.lower().startswith("linux"):
+        cmd = cmd.replace(" --no-headers ", " ")
     p = subprocess.Popen(cmd, shell=1, stdout=subprocess.PIPE)
-    return p.communicate()[0].strip()
+    output = p.communicate()[0].strip()
+    if not sys.platform.lower().startswith("linux"):
+        output = output.split()[1]
+    return output
+
 
 
 class PosixSpecificTestCase(unittest.TestCase):
@@ -27,27 +34,27 @@ class PosixSpecificTestCase(unittest.TestCase):
         kill(self.pid)
 
     def test_process_parent_pid(self):
-        ppid_ps = sh("ps h -o ppid -p %s" %self.pid)
+        ppid_ps = sh("ps --no-headers -o ppid -p %s" %self.pid)
         ppid_psutil = psutil.Process(self.pid).ppid
         self.assertEqual(ppid_ps, str(ppid_psutil))
 
     def test_process_uid(self):
-        uid_ps = sh("ps h -o euid -p %s" %self.pid)
+        uid_ps = sh("ps --no-headers -o euid -p %s" %self.pid)
         uid_psutil = psutil.Process(self.pid).uid
         self.assertEqual(uid_ps, str(uid_psutil))
 
     def test_process_gid(self):
-        gid_ps = sh("ps h -o egid -p %s" %self.pid)
+        gid_ps = sh("ps --no-headers -o egid -p %s" %self.pid)
         gid_psutil = psutil.Process(self.pid).uid
         self.assertEqual(gid_ps, str(gid_psutil))
 
     def test_process_username(self):
-        username_ps = sh("ps h -o user -p %s" %self.pid)
+        username_ps = sh("ps --no-headers -o user -p %s" %self.pid)
         username_psutil = psutil.Process(self.pid).username
         self.assertEqual(username_ps, username_psutil)
 
     def test_process_groupname(self):
-        groupname_ps = sh("ps h -o egroup -p %s" %self.pid)
+        groupname_ps = sh("ps --no-headers -o egroup -p %s" %self.pid)
         groupname_psutil = psutil.Process(self.pid).groupname
         self.assertEqual(groupname_ps, groupname_psutil)
 
@@ -55,7 +62,7 @@ class PosixSpecificTestCase(unittest.TestCase):
         # give python interpreter some time to properly initialize
         # so that the results are the same
         time.sleep(0.1)
-        rss_ps = sh("ps h -o rss -p %s" %self.pid)
+        rss_ps = sh("ps --no-headers -o rss -p %s" %self.pid)
         rss_psutil = psutil.Process(self.pid).get_memory_info()[0] / 1024
         self.assertEqual(rss_ps, str(rss_psutil))
 
@@ -63,12 +70,12 @@ class PosixSpecificTestCase(unittest.TestCase):
         # give python interpreter some time to properly initialize
         # so that the results are the same
         time.sleep(0.1)
-        vsz_ps = sh("ps h -o vsz -p %s" %self.pid)
+        vsz_ps = sh("ps --no-headers -o vsz -p %s" %self.pid)
         vsz_psutil = psutil.Process(self.pid).get_memory_info()[1] / 1024
         self.assertEqual(vsz_ps, str(vsz_psutil))
 
     def test_process_create_time(self):
-        start_ps = sh("ps h -o start -p %s" %self.pid)
+        start_ps = sh("ps --no-headers -o start -p %s" %self.pid)
         start_psutil = psutil.Process(self.pid).create_time
         start_psutil = time.strftime("%H:%M:%S", time.localtime(start_psutil))
         self.assertEqual(start_ps, start_psutil)
