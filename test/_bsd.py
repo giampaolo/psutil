4dafc5e51b5cb3583fbe7e47a25fbddc550fd52e
Giampaolo Rodola' <g.rodola@gmail.com>
2010-01-30 16:49:17 +0000

    moved create_time test into specific bsd test module since 'ps' on freebsd has a different format output

--- test/_bsd.py
+++ test/_bsd.py
@@ -1,15 +1,37 @@
+#!/usr/bin/env python
+
 import unittest
 import subprocess
+import time
 
 import psutil
 
+from test_psutil import kill, PYTHON, DEVNULL
+
 
 class BSDSpecificTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.pid = subprocess.Popen(PYTHON, stdout=DEVNULL, stderr=DEVNULL).pid
+
+    def tearDown(self):
+        kill(self.pid)
 
     def test_TOTAL_PHYMEM(self):
         p = subprocess.Popen('sysctl -a | grep hw.physmem', shell=1, stdout=subprocess.PIPE)
         out = p.stdout.read()
         value = int(out.split()[1])
         self.assertEqual(value, psutil.TOTAL_PHYMEM)
+
+    def test_process_create_time(self):
+        cmdline = "ps -o lstart -p %s" %self.pid
+        p = subprocess.Popen(cmdline, shell=1, stdout=subprocess.PIPE)
+        output = p.communicate()[0]
+        start_ps = output.replace('STARTED', '').strip()
+        start_psutil = psutil.Process(self.pid).create_time
+        start_psutil = time.strftime("%a %b %d %H:%M:%S %Y", 
+                                     time.localtime(start_psutil))
+        self.assertEqual(start_ps, start_psutil)
+
 
 
