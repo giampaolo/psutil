402a9c737c354f189ed09848e34505eb0b24d714
Giampaolo Rodola' <g.rodola@gmail.com>
2011-06-18 14:12:26 +0000

    add killall.py example script

---
+++ examples/killall.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Kill a process by name.
+"""
+
+import os
+import sys
+import psutil
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit('usage: %s name' % __file__)
+    else:
+        NAME = sys.argv[1]
+
+    killed = []
+    for proc in psutil.process_iter():
+        if proc.name == NAME and proc.pid != os.getpid():
+            proc.kill()
+            killed.append(proc.pid)
+    if not killed:
+        sys.exit('%s: no process found' % NAME)
+    else:
+        sys.exit(0)
+
+sys.exit(main())
