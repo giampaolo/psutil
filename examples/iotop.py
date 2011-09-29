30c5cffe25c92c5410eb4d23905b612bb63b1866
Giampaolo Rodola' <g.rodola@gmail.com>
2011-09-29 23:14:46 +0000

    iotop.py refactoring

--- examples/iotop.py
+++ examples/iotop.py
@@ -1,18 +1,32 @@
+#!/usr/bin/env python
+#
+# $Id$
+#
+
+"""
+A clone of iotop (http://guichaz.free.fr/iotop/) showing real time
+disk I/O statistics.
+
+It works on UNIX only as curses module is not available on Windows.
+
+Author: Giampaolo Rodola' <g.rodola@gmail.com>
+"""
+
 import time
 import curses
+import atexit
 
 import psutil
+
+win = curses.initscr()
 
 def bytes2human(n):
     """
     >>> bytes2human(10000)
-    '9.8K'
+    '9.8 K/s'
     >>> bytes2human(100001221)
-    '95.4M'
+    '95.4 M/s'
     """
-    assert isinstance(n, (int, long, float)), n
-    if n == 0:
-        return "0B"
     symbols = ('K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y')
     prefix = {}
     for i, s in enumerate(symbols):
@@ -20,11 +34,15 @@ def bytes2human(n):
     for s in reversed(symbols):
         if n >= prefix[s]:
             value = float(n) / prefix[s]
-            return '%.2f %s' % (value, s)
-#    return "0.0B " % n
+            return '%.2f %s/s' % (value, s)
+    return "0.00 B/s"
 
-
-def poll():
+def poll(interval):
+    """Calculate IO usage by comparing IO statics before and
+    after the interval.
+    Return a tuple including all currently running processes
+    sorted by IO activity and total disks I/O activity.
+    """
     # first get a list of all processes and disk io counters
     procs = [p for p in psutil.process_iter()]
     for p in procs[:]:
@@ -36,7 +54,7 @@ def poll():
     disks_before = psutil.disk_io_counters()
 
     # sleep some time
-    time.sleep(1)
+    time.sleep(interval)
 
     # then retrieve the same info again
     for p in procs[:]:
@@ -60,49 +78,56 @@ def poll():
     disks_read_per_sec = disks_after.read_bytes - disks_before.read_bytes
     disks_write_per_sec = disks_after.write_bytes - disks_before.write_bytes
 
-    # sort processes by total IO
+    # sort processes by total disk IO so that the more intensive
+    # ones get listed first
     processes = sorted(procs, key=lambda p: p._total, reverse=True)
 
     return (processes, disks_read_per_sec, disks_write_per_sec)
 
-
-def run(myscreen):
+def run(win):
+    """Print results on screen by using curses."""
     curses.endwin()
     templ = "%-5s %-7s %11s %11s  %s"
+    interval = 0
     while 1:
-        procs, disks_read, disks_write = poll()
-        myscreen.erase()
-        #
+        procs, disks_read, disks_write = poll(interval)
+        win.erase()
+
         disks_tot = "Total DISK READ: %s | Total DISK WRITE: %s" \
                     % (bytes2human(disks_read), bytes2human(disks_write))
-        myscreen.addstr(0, 0, disks_tot)
-        #
+        win.addstr(0, 0, disks_tot)
+
         header = templ % ("PID", "USER", "DISK READ", "DISK WRITE", "COMMAND")
-        header += " " * (myscreen.getmaxyx()[1] - len(header))
-        myscreen.addstr(1, 0, header, curses.A_REVERSE)
-        #
+        header += " " * (win.getmaxyx()[1] - len(header))
+        win.addstr(1, 0, header, curses.A_REVERSE)
+
         lineno = 2
         for p in procs:
             line = templ % (p.pid,
                             p._username[:7],
-                            bytes2human(p._read_per_sec) + '/s',
-                            bytes2human(p._write_per_sec) + '/s',
+                            bytes2human(p._read_per_sec),
+                            bytes2human(p._write_per_sec),
                             p._cmdline)
             try:
-                myscreen.addstr(lineno, 0, line)
+                win.addstr(lineno, 0, line)
             except curses.error:
                 break
-            myscreen.refresh()
+            win.refresh()
             lineno += 1
+        interval = 1
 
 def main():
-    myscreen = curses.initscr()
-    try:
-        run(myscreen)
-    except (KeyboardInterrupt, SystemExit):
-        pass
-    finally:
-        curses.nocbreak(); myscreen.keypad(0); curses.echo()
+    def tear_down():
+        win.keypad(0)
+        curses.nocbreak()
+        curses.echo()
         curses.endwin()
 
-main()
+    atexit.register(tear_down)
+    try:
+        run(win)
+    except (KeyboardInterrupt, SystemExit):
+        pass
+
+if __name__ == '__main__':
+    main()
