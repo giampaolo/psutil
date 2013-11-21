45b587a39d20979455d023d865268ed711124651
Giampaolo Rodola' <g.rodola@gmail.com>
2013-11-21 20:53:49 +0100

    pep8ify

--- psutil/_common.py
+++ psutil/_common.py
@@ -7,13 +7,48 @@
 """Common objects shared by all _ps* modules."""
 
 from __future__ import division
-import sys
-import os
-import stat
 import errno
+import os
+import socket
+import stat
+import sys
 import warnings
 
+from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
+
 from psutil._compat import namedtuple, wraps
+
+# --- constants
+
+AF_INET6 = getattr(socket, 'AF_INET6', None)
+AF_UNIX = getattr(socket, 'AF_UNIX', None)
+
+STATUS_RUNNING = "running"
+STATUS_SLEEPING = "sleeping"
+STATUS_DISK_SLEEP = "disk-sleep"
+STATUS_STOPPED = "stopped"
+STATUS_TRACING_STOP = "tracing-stop"
+STATUS_ZOMBIE = "zombie"
+STATUS_DEAD = "dead"
+STATUS_WAKE_KILL = "wake-kill"
+STATUS_WAKING = "waking"
+STATUS_IDLE = "idle"  # BSD
+STATUS_LOCKED = "locked"  # BSD
+STATUS_WAITING = "waiting"  # BSD
+
+CONN_ESTABLISHED = "ESTABLISHED"
+CONN_SYN_SENT = "SYN_SENT"
+CONN_SYN_RECV = "SYN_RECV"
+CONN_FIN_WAIT1 = "FIN_WAIT1"
+CONN_FIN_WAIT2 = "FIN_WAIT2"
+CONN_TIME_WAIT = "TIME_WAIT"
+CONN_CLOSE = "CLOSE"
+CONN_CLOSE_WAIT = "CLOSE_WAIT"
+CONN_LAST_ACK = "LAST_ACK"
+CONN_LISTEN = "LISTEN"
+CONN_CLOSING = "CLOSING"
+CONN_NONE = "NONE"
+
 
 # --- functions
 
@@ -28,6 +63,7 @@ def usage_percent(used, total, _round=None):
     else:
         return ret
 
+
 def memoize(f):
     """A simple memoize decorator for functions."""
     cache = {}
@@ -36,6 +72,7 @@ def memoize(f):
             cache[x] = f(*x)
         return cache[x]
     return memf
+
 
 class cached_property(object):
     """A memoize decorator for class properties."""
@@ -49,6 +86,7 @@ class cached_property(object):
         if self.enabled:
             instance.__dict__[self.func.__name__] = ret
         return ret
+
 
 # http://goo.gl/jYLvf
 def deprecated(replacement=None):
@@ -85,63 +123,29 @@ def isfile_strict(path):
         return stat.S_ISREG(st.st_mode)
 
 
-# --- constants
-
-STATUS_RUNNING = "running"
-STATUS_SLEEPING = "sleeping"
-STATUS_DISK_SLEEP = "disk-sleep"
-STATUS_STOPPED = "stopped"
-STATUS_TRACING_STOP = "tracing-stop"
-STATUS_ZOMBIE = "zombie"
-STATUS_DEAD = "dead"
-STATUS_WAKE_KILL = "wake-kill"
-STATUS_WAKING = "waking"
-STATUS_IDLE = "idle"  # BSD
-STATUS_LOCKED = "locked"  # BSD
-STATUS_WAITING = "waiting"  # BSD
-
-CONN_ESTABLISHED = "ESTABLISHED"
-CONN_SYN_SENT = "SYN_SENT"
-CONN_SYN_RECV = "SYN_RECV"
-CONN_FIN_WAIT1 = "FIN_WAIT1"
-CONN_FIN_WAIT2 = "FIN_WAIT2"
-CONN_TIME_WAIT = "TIME_WAIT"
-CONN_CLOSE = "CLOSE"
-CONN_CLOSE_WAIT = "CLOSE_WAIT"
-CONN_LAST_ACK = "LAST_ACK"
-CONN_LISTEN = "LISTEN"
-CONN_CLOSING = "CLOSING"
-CONN_NONE = "NONE"
-
 # --- Process.get_connections() 'kind' parameter mapping
 
-import socket
-from socket import AF_INET, SOCK_STREAM, SOCK_DGRAM
-AF_INET6 = getattr(socket, 'AF_INET6', None)
-AF_UNIX = getattr(socket, 'AF_UNIX', None)
-
 conn_tmap = {
-    "all"  : ([AF_INET, AF_INET6, AF_UNIX], [SOCK_STREAM, SOCK_DGRAM]),
-    "tcp"  : ([AF_INET, AF_INET6], [SOCK_STREAM]),
-    "tcp4" : ([AF_INET],           [SOCK_STREAM]),
-    "udp"  : ([AF_INET, AF_INET6], [SOCK_DGRAM]),
-    "udp4" : ([AF_INET],           [SOCK_DGRAM]),
-    "inet" : ([AF_INET, AF_INET6], [SOCK_STREAM, SOCK_DGRAM]),
+    "all":   ([AF_INET, AF_INET6, AF_UNIX], [SOCK_STREAM, SOCK_DGRAM]),
+    "tcp":   ([AF_INET, AF_INET6], [SOCK_STREAM]),
+    "tcp4":  ([AF_INET],           [SOCK_STREAM]),
+    "udp":   ([AF_INET, AF_INET6], [SOCK_DGRAM]),
+    "udp4":  ([AF_INET],           [SOCK_DGRAM]),
+    "inet":  ([AF_INET, AF_INET6], [SOCK_STREAM, SOCK_DGRAM]),
     "inet4": ([AF_INET],           [SOCK_STREAM, SOCK_DGRAM]),
     "inet6": ([AF_INET6],          [SOCK_STREAM, SOCK_DGRAM]),
 }
 
 if AF_INET6 is not None:
     conn_tmap.update({
-        "tcp6" : ([AF_INET6],          [SOCK_STREAM]),
-        "udp6" : ([AF_INET6],          [SOCK_DGRAM]),
+        "tcp6": ([AF_INET6],       [SOCK_STREAM]),
+        "udp6": ([AF_INET6],       [SOCK_DGRAM]),
     })
 
 if AF_UNIX is not None:
     conn_tmap.update({
-        "unix" : ([AF_UNIX],           [SOCK_STREAM, SOCK_DGRAM]),
+        "unix": ([AF_UNIX],        [SOCK_STREAM, SOCK_DGRAM]),
     })
-
 
 del AF_INET, AF_INET6, AF_UNIX, SOCK_STREAM, SOCK_DGRAM, socket
 
@@ -153,9 +157,12 @@ nt_sysmeminfo = namedtuple('usage', 'total used free percent')
 nt_swapmeminfo = namedtuple('swap', 'total used free percent sin sout')
 nt_diskinfo = namedtuple('usage', 'total used free percent')
 nt_partition = namedtuple('partition',  'device mountpoint fstype opts')
-nt_net_iostat = namedtuple('iostat',
+nt_net_iostat = namedtuple(
+    'iostat',
     'bytes_sent bytes_recv packets_sent packets_recv errin errout dropin dropout')
-nt_disk_iostat = namedtuple('iostat', 'read_count write_count read_bytes write_bytes read_time write_time')
+nt_disk_iostat = namedtuple(
+    'iostat',
+    'read_count write_count read_bytes write_bytes read_time write_time')
 nt_user = namedtuple('user', 'name terminal host started')
 
 # processes
@@ -169,18 +176,22 @@ nt_io = namedtuple('io', 'read_count write_count read_bytes write_bytes')
 nt_ionice = namedtuple('ionice', 'ioclass value')
 nt_ctxsw = namedtuple('amount', 'voluntary involuntary')
 
+
+# --- misc
+
+# backward compatibility layer for Process.get_connections() ntuple
 class nt_connection(namedtuple('connection',
                                'fd family type laddr raddr status')):
     __slots__ = ()
 
     @property
     def local_address(self):
-        warnings.warn("'local_address' field is deprecated; use 'laddr'" \
+        warnings.warn("'local_address' field is deprecated; use 'laddr'"
                       "instead", category=DeprecationWarning, stacklevel=2)
         return self.laddr
 
     @property
     def remote_address(self):
-        warnings.warn("'remote_address' field is deprecated; use 'raddr'" \
+        warnings.warn("'remote_address' field is deprecated; use 'raddr'"
                       "instead", category=DeprecationWarning, stacklevel=2)
         return self.raddr
