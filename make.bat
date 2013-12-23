b44abc2e80937747fe8d85a8239ec1b05105fe1c
Giampaolo Rodola' <g.rodola@gmail.com>
2013-12-23 12:04:34 -0800

    make.bat: exit on error

--- make.bat
+++ make.bat
@@ -2,15 +2,14 @@
 
 REM ==========================================================================
 REM Shortcuts for various tasks, emulating UNIX "make".
-REM Use this to script for various tasks such as runing tests, install
-REM psutil, etc.
+REM Use this script for various tasks such as running tests or install psutil.
 REM To run tests run "make.bat test".
-REM This script is modeled after my Windows installation so it might
-REM need some adjustements in order to work on your system.
+REM This script is modeled after my Windows installation so it might need some
+REM adjustements in order to work on your system.
 REM The following assumptions are made:
-REM - Python is installed in C:\PythonXY\python.exe
+REM - the default Python interpreter is in C:\Python27\python.exe
 REM - the right Visual Studio version is installed
-REM - in case of Python 2.4 and 2.5 use mingw32 compiler
+REM - in case of Python 2.4 and 2.5 mingw32 compiler is used
 REM ==========================================================================
 
 
@@ -78,12 +77,13 @@ if "%1" == "test" (
 if "%1" == "create-exes" (
     :create-exes
     call :clean
-    C:\Python26\python.exe setup.py build bdist_wininst
-    if %errorlevel% neq 0 goto :error
-    C:\Python27\python.exe setup.py build bdist_wininst
-    if %errorlevel% neq 0 goto :error
-    C:\Python33\python.exe setup.py build bdist_wininst
-    if %errorlevel% neq 0 goto :error
+
+    for %%x in ("C:\Python26\python.exe setup.py build bdist_wininst"
+                "C:\Python27\python.exe setup.py build bdist_wininst") do (
+        set myvar=%%x
+        %myvar%
+    )
+
     goto :eof
 )
 
