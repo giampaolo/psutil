e377415603b2448c97da17ed1f3efc320fd60176
Giampaolo Rodola' <g.rodola@gmail.com>
2013-12-26 08:48:14 -0800

    use unittest.skipIf() decorator instead of old register_warning()

--- test/_windows.py
+++ test/_windows.py
@@ -15,22 +15,18 @@ import sys
 import time
 import traceback
 
-from test_psutil import (register_warning, get_test_subprocess, wait_for_pid,
-                         reap_children, unittest)
+from test_psutil import (get_test_subprocess, wait_for_pid, reap_children,
+                         unittest)
 
 try:
     import wmi
 except ImportError:
-    err = sys.exc_info()[1]
-    register_warning("Couldn't run wmi tests: %s" % str(err))
     wmi = None
 try:
     import win32api
     import win32con
 except ImportError:
-    err = sys.exc_info()[1]
-    register_warning("Couldn't run pywin32 tests: %s" % str(err))
-    win32api = None
+    win32api = win32con = None
 
 from psutil._compat import PY3, callable, long
 from psutil._pswindows import ACCESS_DENIED_SET
@@ -83,7 +79,7 @@ class WindowsSpecificTestCase(unittest.TestCase):
         else:
             self.assertTrue(rss > 0)
 
-    def test_signal(self):
+    def test_send_signal(self):
         p = psutil.Process(self.pid)
         self.assertRaises(ValueError, p.send_signal, signal.SIGINT)
 
@@ -107,175 +103,185 @@ class WindowsSpecificTestCase(unittest.TestCase):
             except psutil.Error:
                 pass
 
-    if wmi is not None:
+    # --- Process class tests
 
-        # --- Process class tests
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_name(self):
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        self.assertEqual(p.name, w.Caption)
 
-        def test_process_name(self):
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            self.assertEqual(p.name, w.Caption)
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_exe(self):
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        self.assertEqual(p.exe, w.ExecutablePath)
 
-        def test_process_exe(self):
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            self.assertEqual(p.exe, w.ExecutablePath)
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_cmdline(self):
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        self.assertEqual(' '.join(p.cmdline),
+                         w.CommandLine.replace('"', ''))
 
-        def test_process_cmdline(self):
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            self.assertEqual(' '.join(p.cmdline),
-                             w.CommandLine.replace('"', ''))
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_username(self):
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        domain, _, username = w.GetOwner()
+        username = "%s\\%s" % (domain, username)
+        self.assertEqual(p.username, username)
 
-        def test_process_username(self):
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            domain, _, username = w.GetOwner()
-            username = "%s\\%s" % (domain, username)
-            self.assertEqual(p.username, username)
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_rss_memory(self):
+        time.sleep(0.1)
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        rss = p.memory_info().rss
+        self.assertEqual(rss, int(w.WorkingSetSize))
 
-        def test_process_rss_memory(self):
-            time.sleep(0.1)
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            rss = p.memory_info().rss
-            self.assertEqual(rss, int(w.WorkingSetSize))
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_vms_memory(self):
+        time.sleep(0.1)
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        vms = p.memory_info().vms
+        # http://msdn.microsoft.com/en-us/library/aa394372(VS.85).aspx
+        # ...claims that PageFileUsage is represented in Kilo
+        # bytes but funnily enough on certain platforms bytes are
+        # returned instead.
+        wmi_usage = int(w.PageFileUsage)
+        if (vms != wmi_usage) and (vms != wmi_usage * 1024):
+            self.fail("wmi=%s, psutil=%s" % (wmi_usage, vms))
 
-        def test_process_vms_memory(self):
-            time.sleep(0.1)
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            vms = p.memory_info().vms
-            # http://msdn.microsoft.com/en-us/library/aa394372(VS.85).aspx
-            # ...claims that PageFileUsage is represented in Kilo
-            # bytes but funnily enough on certain platforms bytes are
-            # returned instead.
-            wmi_usage = int(w.PageFileUsage)
-            if (vms != wmi_usage) and (vms != wmi_usage * 1024):
-                self.fail("wmi=%s, psutil=%s" % (wmi_usage, vms))
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_process_create_time(self):
+        w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+        p = psutil.Process(self.pid)
+        wmic_create = str(w.CreationDate.split('.')[0])
+        psutil_create = time.strftime("%Y%m%d%H%M%S",
+                                      time.localtime(p.create_time))
+        self.assertEqual(wmic_create, psutil_create)
 
-        def test_process_create_time(self):
-            w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-            p = psutil.Process(self.pid)
-            wmic_create = str(w.CreationDate.split('.')[0])
-            psutil_create = time.strftime("%Y%m%d%H%M%S",
-                                          time.localtime(p.create_time))
-            self.assertEqual(wmic_create, psutil_create)
+    # --- psutil namespace functions and constants tests
 
-        # --- psutil namespace functions and constants tests
-        @unittest.skipUnless(hasattr(os, 'NUMBER_OF_PROCESSORS'),
-                             'NUMBER_OF_PROCESSORS env var is not available')
-        def test_cpu_count(self):
-            num_cpus = int(os.environ['NUMBER_OF_PROCESSORS'])
-            self.assertEqual(num_cpus, psutil.cpu_count())
+    @unittest.skipUnless(hasattr(os, 'NUMBER_OF_PROCESSORS'),
+                         'NUMBER_OF_PROCESSORS env var is not available')
+    def test_cpu_count(self):
+        num_cpus = int(os.environ['NUMBER_OF_PROCESSORS'])
+        self.assertEqual(num_cpus, psutil.cpu_count())
 
-        def test_total_phymem(self):
-            w = wmi.WMI().Win32_ComputerSystem()[0]
-            self.assertEqual(int(w.TotalPhysicalMemory),
-                             psutil.virtual_memory().total)
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_total_phymem(self):
+        w = wmi.WMI().Win32_ComputerSystem()[0]
+        self.assertEqual(int(w.TotalPhysicalMemory),
+                         psutil.virtual_memory().total)
 
-        # def test__UPTIME(self):
-        #     # _UPTIME constant is not public but it is used internally
-        #     # as value to return for pid 0 creation time.
-        #     # WMI behaves the same.
-        #     w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
-        #     p = psutil.Process(0)
-        #     wmic_create = str(w.CreationDate.split('.')[0])
-        #     psutil_create = time.strftime("%Y%m%d%H%M%S",
-        #                                   time.localtime(p.create_time))
-        #
+    # @unittest.skipIf(wmi is None, "wmi module is not installed")
+    # def test__UPTIME(self):
+    #     # _UPTIME constant is not public but it is used internally
+    #     # as value to return for pid 0 creation time.
+    #     # WMI behaves the same.
+    #     w = wmi.WMI().Win32_Process(ProcessId=self.pid)[0]
+    #     p = psutil.Process(0)
+    #     wmic_create = str(w.CreationDate.split('.')[0])
+    #     psutil_create = time.strftime("%Y%m%d%H%M%S",
+    #                                   time.localtime(p.create_time))
+    #
 
-        def test_pids(self):
-            # Note: this test might fail if the OS is starting/killing
-            # other processes in the meantime
-            w = wmi.WMI().Win32_Process()
-            wmi_pids = [x.ProcessId for x in w]
-            wmi_pids.sort()
-            psutil_pids = psutil.pids()
-            psutil_pids.sort()
-            if wmi_pids != psutil_pids:
-                difference = \
-                    filter(lambda x: x not in wmi_pids, psutil_pids) + \
-                    filter(lambda x: x not in psutil_pids, wmi_pids)
-                self.fail("difference: " + str(difference))
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_pids(self):
+        # Note: this test might fail if the OS is starting/killing
+        # other processes in the meantime
+        w = wmi.WMI().Win32_Process()
+        wmi_pids = [x.ProcessId for x in w]
+        wmi_pids.sort()
+        psutil_pids = psutil.pids()
+        psutil_pids.sort()
+        if wmi_pids != psutil_pids:
+            difference = \
+                filter(lambda x: x not in wmi_pids, psutil_pids) + \
+                filter(lambda x: x not in psutil_pids, wmi_pids)
+            self.fail("difference: " + str(difference))
 
-        def test_disks(self):
-            ps_parts = psutil.disk_partitions(all=True)
-            wmi_parts = wmi.WMI().Win32_LogicalDisk()
-            for ps_part in ps_parts:
-                for wmi_part in wmi_parts:
-                    if ps_part.device.replace('\\', '') == wmi_part.DeviceID:
-                        if not ps_part.mountpoint:
-                            # this is usually a CD-ROM with no disk inserted
-                            break
-                        try:
-                            usage = psutil.disk_usage(ps_part.mountpoint)
-                        except OSError:
-                            err = sys.exc_info()[1]
-                            if err.errno == errno.ENOENT:
-                                # usually this is the floppy
-                                break
-                            else:
-                                raise
-                        self.assertEqual(usage.total, int(wmi_part.Size))
-                        wmi_free = int(wmi_part.FreeSpace)
-                        self.assertEqual(usage.free, wmi_free)
-                        # 10 MB tollerance
-                        if abs(usage.free - wmi_free) > 10 * 1024 * 1024:
-                            self.fail("psutil=%s, wmi=%s" % (
-                                usage.free, wmi_free))
+    @unittest.skipIf(wmi is None, "wmi module is not installed")
+    def test_disks(self):
+        ps_parts = psutil.disk_partitions(all=True)
+        wmi_parts = wmi.WMI().Win32_LogicalDisk()
+        for ps_part in ps_parts:
+            for wmi_part in wmi_parts:
+                if ps_part.device.replace('\\', '') == wmi_part.DeviceID:
+                    if not ps_part.mountpoint:
+                        # this is usually a CD-ROM with no disk inserted
                         break
-                else:
-                    self.fail("can't find partition %s" % repr(ps_part))
-
-    if win32api is not None:
-
-        def test_num_handles(self):
-            p = psutil.Process(os.getpid())
-            before = p.num_handles()
-            handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION,
-                                          win32con.FALSE, os.getpid())
-            after = p.num_handles()
-            self.assertEqual(after, before + 1)
-            win32api.CloseHandle(handle)
-            self.assertEqual(p.num_handles(), before)
-
-        def test_num_handles_2(self):
-            # Note: this fails from time to time; I'm keen on thinking
-            # it doesn't mean something is broken
-            def call(p, attr):
-                attr = getattr(p, name, None)
-                if attr is not None and callable(attr):
-                    attr()
-                else:
-                    attr
-
-            p = psutil.Process(self.pid)
-            failures = []
-            for name in dir(psutil.Process):
-                if name.startswith('_') \
-                    or name.startswith('set_') \
-                    or name.startswith('get')  \
-                    or name in ('terminate', 'kill', 'suspend', 'resume',
-                                'nice', 'send_signal', 'wait', 'children',
-                                'as_dict'):
-                    continue
-                else:
                     try:
-                        call(p, name)
-                        num1 = p.num_handles()
-                        call(p, name)
-                        num2 = p.num_handles()
-                    except (psutil.NoSuchProcess, psutil.AccessDenied):
-                        pass
-                    else:
-                        if num2 > num1:
-                            fail = \
-                                "failure while processing Process.%s method " \
-                                "(before=%s, after=%s)" % (name, num1, num2)
-                            failures.append(fail)
-            if failures:
-                self.fail('\n' + '\n'.join(failures))
+                        usage = psutil.disk_usage(ps_part.mountpoint)
+                    except OSError:
+                        err = sys.exc_info()[1]
+                        if err.errno == errno.ENOENT:
+                            # usually this is the floppy
+                            break
+                        else:
+                            raise
+                    self.assertEqual(usage.total, int(wmi_part.Size))
+                    wmi_free = int(wmi_part.FreeSpace)
+                    self.assertEqual(usage.free, wmi_free)
+                    # 10 MB tollerance
+                    if abs(usage.free - wmi_free) > 10 * 1024 * 1024:
+                        self.fail("psutil=%s, wmi=%s" % (
+                            usage.free, wmi_free))
+                    break
+            else:
+                self.fail("can't find partition %s" % repr(ps_part))
+
+    @unittest.skipIf(win32api is None, "pywin32 module is not installed")
+    def test_num_handles(self):
+        p = psutil.Process(os.getpid())
+        before = p.num_handles()
+        handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION,
+                                      win32con.FALSE, os.getpid())
+        after = p.num_handles()
+        self.assertEqual(after, before + 1)
+        win32api.CloseHandle(handle)
+        self.assertEqual(p.num_handles(), before)
+
+    @unittest.skipIf(win32api is None, "pywin32 module is not installed")
+    def test_num_handles_2(self):
+        # Note: this fails from time to time; I'm keen on thinking
+        # it doesn't mean something is broken
+        def call(p, attr):
+            attr = getattr(p, name, None)
+            if attr is not None and callable(attr):
+                attr()
+            else:
+                attr
+
+        p = psutil.Process(self.pid)
+        failures = []
+        for name in dir(psutil.Process):
+            if name.startswith('_') \
+                or name.startswith('set_') \
+                or name.startswith('get')  \
+                or name in ('terminate', 'kill', 'suspend', 'resume',
+                            'nice', 'send_signal', 'wait', 'children',
+                            'as_dict'):
+                continue
+            else:
+                try:
+                    call(p, name)
+                    num1 = p.num_handles()
+                    call(p, name)
+                    num2 = p.num_handles()
+                except (psutil.NoSuchProcess, psutil.AccessDenied):
+                    pass
+                else:
+                    if num2 > num1:
+                        fail = \
+                            "failure while processing Process.%s method " \
+                            "(before=%s, after=%s)" % (name, num1, num2)
+                        failures.append(fail)
+        if failures:
+            self.fail('\n' + '\n'.join(failures))
 
 
 class TestDualProcessImplementation(unittest.TestCase):
