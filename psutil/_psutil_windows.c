5b53f247669ace19c05f4b9ae91336746e00cf6c
Giampaolo Rodola' <g.rodola@gmail.com>
2013-12-26 07:29:20 -0800

    fix some compiler warnings on windows

--- psutil/_psutil_windows.c
+++ psutil/_psutil_windows.c
@@ -2478,7 +2478,9 @@ static char *get_drive_type(int type)
 }
 
 
+#ifndef _ARRAYSIZE
 #define _ARRAYSIZE(a) (sizeof(a)/sizeof(a[0]))
+#endif
 
 /*
  * Return disk partitions as a list of tuples such as
