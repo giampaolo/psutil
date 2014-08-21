37f56ffa74b560361f42d73c2d18306dbdfdcd84
Jeff Tang <mrjefftang@gmail.com>
2014-08-21 11:26:57 -0400

    Special case Process.exe for PID 0 & 4
    Solves issue #528 and #414

--- psutil/_pswindows.py
+++ psutil/_pswindows.py
@@ -245,6 +245,13 @@ class Process(object):
         # Note: os.path.exists(path) may return False even if the file
         # is there, see:
         # http://stackoverflow.com/questions/3112546/os-path-exists-lies
+
+        # see https://github.com/giampaolo/psutil/issues/414
+        # see https://github.com/giampaolo/psutil/issues/528
+        if self.pid == 0:
+            raise AccessDenied(self.pid, self._name)
+        elif self.pid == 4:
+            raise AccessDenied(self.pid, self._name)
         return _convert_raw_path(cext.proc_exe(self.pid))
 
     @wrap_exceptions
