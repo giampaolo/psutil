1174a608b8d98cb9334f71860eaae85d398c5020
Landry Breuil <landry@openbsd.org>
2014-12-28 13:21:14 +0100

    Implement psutil_per_cpu_times()

--- psutil/_psutil_openbsd.c
+++ psutil/_psutil_openbsd.c
@@ -1164,7 +1164,7 @@ static PyObject *
 psutil_per_cpu_times(PyObject *self, PyObject *args)
 {
     static int maxcpus;
-    int mib[2];
+    int mib[3];
     int ncpu;
     size_t len;
     size_t size;
@@ -1184,25 +1184,27 @@ psutil_per_cpu_times(PyObject *self, PyObject *args)
         PyErr_SetFromErrno(PyExc_OSError);
         goto error;
     }
-    long cpu_time[ncpu][CPUSTATES];
-
-    // per-cpu info
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_CPTIME;
-    size = sizeof(cpu_time);
-    if (sysctl(mib, 2, &cpu_time, &size, NULL, 0) == -1) {
-        PyErr_SetFromErrno(PyExc_OSError);
-        return NULL;
-    }
+    long cpu_time[CPUSTATES];
 
     for (i = 0; i < ncpu; i++) {
+        // per-cpu info
+        mib[0] = CTL_KERN;
+        mib[1] = KERN_CPTIME2;
+        mib[2] = i;
+        size = sizeof(cpu_time);
+        if (sysctl(mib, 3, &cpu_time, &size, NULL, 0) == -1) {
+            warn("failed to get kern.cptime2");
+            PyErr_SetFromErrno(PyExc_OSError);
+            return NULL;
+        }
+
         py_cputime = Py_BuildValue(
             "(ddddd)",
-            (double)cpu_time[i][CP_USER] / CLOCKS_PER_SEC,
-            (double)cpu_time[i][CP_NICE] / CLOCKS_PER_SEC,
-            (double)cpu_time[i][CP_SYS] / CLOCKS_PER_SEC,
-            (double)cpu_time[i][CP_IDLE] / CLOCKS_PER_SEC,
-            (double)cpu_time[i][CP_INTR] / CLOCKS_PER_SEC);
+            (double)cpu_time[CP_USER] / CLOCKS_PER_SEC,
+            (double)cpu_time[CP_NICE] / CLOCKS_PER_SEC,
+            (double)cpu_time[CP_SYS] / CLOCKS_PER_SEC,
+            (double)cpu_time[CP_IDLE] / CLOCKS_PER_SEC,
+            (double)cpu_time[CP_INTR] / CLOCKS_PER_SEC);
         if (!py_cputime)
             goto error;
         if (PyList_Append(py_retlist, py_cputime))
