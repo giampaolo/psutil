87384bab5e6324c3142fdd70eed78c5a18da0854
Karthikkumar Namasivayam <knamasi@L-IDC2A1DKQ2-M.local>
2015-01-01 19:45:50 +0530

    adding example pstree.py with some fixes

--- examples/pidof.py
+++ examples/pidof.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python
 
+# Copyright (c) 2009, Giampaolo Rodola', karthikrev. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
 """
 A clone of 'pidof' cmdline utility.
 $ pidof  /usr/bin/python
 1140 1138 1136 1134 1133 1129 1127 1125 1121 1120 1119
 """
 
+from __future__ import print_function
 import psutil
 import sys
 
 
 def pidsof(pgm):
     pids = []
+    # Iterate on all proccesses and find matching cmdline and get list of
+    # corresponding PIDs
     for proc in psutil.process_iter():
-        process = proc.as_dict()
-        if len(process['cmdline']) > 0 and process['cmdline'][0] == pgm:
-            pids.append(str(process['pid']))
+        try:
+            cmdline = proc.cmdline()
+            pid = proc.pid
+        except psutil.Error:
+            continue
+        if len(cmdline) > 0 and cmdline[0] == pgm:
+            pids.append(str(pid))
     return pids
 
 
@@ -25,7 +37,7 @@ def main():
     else:
         pgmname = sys.argv[1]
     pids = pidsof(pgmname)
-    print " ".join(pids)
+    print(" ".join(pids))
 
 if __name__ == '__main__':
     main()
