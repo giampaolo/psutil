de9f48b3ddb8f0c15e5d30cff38a8e2cdc6e8ef9
Giampaolo Rodola <g.rodola@gmail.com>
2015-02-07 00:23:30 +0100

    #376: add another test for verifying the returned NIC names

--- test/_linux.py
+++ test/_linux.py
@@ -7,6 +7,7 @@
 """Linux specific tests.  These are implicitly run by test_psutil.py."""
 
 from __future__ import division
+import array
 import contextlib
 import fcntl
 import os
@@ -26,8 +27,12 @@ import psutil
 from psutil._compat import PY3
 
 
+SIOCGIFADDR = 0x8915
+SIOCGIFCONF = 0x8912
+SIOCGIFHWADDR = 0x8927
+
+
 def get_ipv4_address(ifname):
-    SIOCGIFADDR = 0x8915
     ifname = ifname[:15]
     if PY3:
         ifname = bytes(ifname, 'ascii')
@@ -40,7 +45,6 @@ def get_ipv4_address(ifname):
 
 
 def get_mac_address(ifname):
-    SIOCGIFHWADDR = 0x8927
     ifname = ifname[:15]
     if PY3:
         ifname = bytes(ifname, 'ascii')
@@ -54,6 +58,30 @@ def get_mac_address(ifname):
             import __builtin__
             ord = __builtin__.ord
         return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1]
+
+
+def get_nic_names():
+    """Find the names of all available network interfaces."""
+    # Thanks to:
+    # https://code.fluendo.com/flumotion/svn/flumotion/trunk/flumotion/common/
+    #     netutils.py
+    ptr_size = len(struct.pack('P', 0))
+    size = 24 + 2 * (ptr_size)
+    max_possible = 128
+    inbytes = max_possible * size
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    with contextlib.closing(s):
+        names = array.array('B', b'\0' * inbytes)
+        outbytes = struct.unpack('iP', fcntl.ioctl(
+            s.fileno(),
+            SIOCGIFCONF,
+            struct.pack('iP', inbytes, names.buffer_info()[0])))[0]
+        namestr = names.tostring()
+        ret = [namestr[i:i + size].split(b'\0', 1)[0]
+               for i in range(0, outbytes, size)]
+        if PY3:
+            ret = [x.decode() for x in ret]
+        return ret
 
 
 class LinuxSpecificTestCase(unittest.TestCase):
@@ -199,6 +227,10 @@ class LinuxSpecificTestCase(unittest.TestCase):
                 self.assertIn(name, nics.keys())
         self.assertEqual(len(nics), found, msg="%s\n---\n%s" % (
             pprint.pformat(nics), out))
+
+    def test_net_if_names_2(self):
+        self.assertEqual(sorted(get_nic_names()),
+                         sorted(psutil.net_if_addrs()))
 
     # --- tests for specific kernel versions
 
