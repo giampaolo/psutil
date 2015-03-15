730835605135a43ee9742e1d4db0e981719b4a53
Arnon Yaari <arnony@infinidat.com>
2015-03-15 22:28:21 +0200

    use 'with open' to make sure file is closed

--- test/test_psutil.py
+++ test/test_psutil.py
@@ -712,7 +712,9 @@ class TestSystemAPIs(unittest.TestCase):
         self.assertEqual(logical, len(psutil.cpu_times(percpu=True)))
         self.assertGreaterEqual(logical, 1)
         #
-        if "physical id" not in open("/proc/cpuinfo").read():
+        with open("/proc/cpuinfo") as fd:
+            cpuinfo_data = fd.read()
+        if "physical id" not in cpuinfo_data:
             raise unittest.SkipTest("cpuinfo doesn't include physical id")
         physical = psutil.cpu_count(logical=False)
         self.assertGreaterEqual(physical, 1)
