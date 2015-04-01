9550a24360a2e8149e466a2012f4686a4d8b87a0
John Burnett <johnburnett@johnburnett.com>
2015-03-31 19:36:17 -0700

    Switch cpu_count_physical/logical to use shared /proc/cpuinfo parsing function.

--- psutil/_pslinux.py
+++ psutil/_pslinux.py
@@ -217,6 +217,29 @@ def swap_memory():
 
 # --- CPUs
 
+def _parse_cpuinfo():
+    """Return a list of dicts, one per processor entry in /proc/cpuinfo.
+    Each dict represents the key/value pairs for a given processor.
+    """
+    current_info = {}
+    proc_infos = [current_info]
+    with open('/proc/cpuinfo', 'rb') as f:
+        for line in f:
+            line = line.strip().lower()
+            if line:
+                line_info = line.split(':', 1)
+                if len(line_info) == 2:
+                    info_key = line_info[0].strip()
+                    info_value = line_info[1].strip()
+                    current_info[info_key] = info_value
+            else:
+                current_info = {}
+                proc_infos.append(current_info)
+    if proc_infos and not proc_infos[-1]:
+        proc_infos.pop()
+    return proc_infos
+
+
 def cpu_times():
     """Return a named tuple representing the following system-wide
     CPU times:
@@ -255,11 +278,8 @@ def cpu_count_logical():
         return os.sysconf("SC_NPROCESSORS_ONLN")
     except ValueError:
         # as a second fallback we try to parse /proc/cpuinfo
-        num = 0
-        with open('/proc/cpuinfo', 'rb') as f:
-            for line in f:
-                if line.lower().startswith(b'processor'):
-                    num += 1
+        proc_infos = _parse_cpuinfo()
+        num = len(proc_infos)
 
         # unknown format (e.g. amrel/sparc architectures), see:
         # https://github.com/giampaolo/psutil/issues/200
@@ -280,31 +300,12 @@ def cpu_count_logical():
 
 def cpu_count_physical():
     """Return the number of physical cores in the system."""
-    with open('/proc/cpuinfo', 'rb') as f:
-        # For each 'physical id' found, find its corresponding
-        # 'cpu cores' count and then total them up.
-        proc_core_counts = {}
-        current_proc_id = None
-        current_core_count = 0
-        for line in f:
-            line = line.lower().strip()
-            if line:
-                line_info = line.split(':')
-                if len(line_info) == 2:
-                    info_key, info_value = line_info
-                    if info_key.startswith(b'physical id'):
-                        current_proc_id = info_value.strip()
-                    if info_key.startswith(b'cpu cores'):
-                        current_core_count = int(info_value)
-            else:
-                # New section of cpuinfo
-                current_proc_id = None
-                current_core_count = 0
-
-            if current_proc_id and current_core_count:
-                proc_core_counts[current_proc_id] = current_core_count
-                current_proc_id = None
-                current_core_count = 0
+    proc_infos = _parse_cpuinfo()
+    proc_core_counts = {}
+    for proc_info in proc_infos:
+        proc_id = proc_info.get(b'physical id')
+        if proc_id:
+            proc_core_counts[proc_id] = int(proc_info.get(b'cpu cores', 0))
 
     if proc_core_counts:
         return sum(proc_core_counts.values())
