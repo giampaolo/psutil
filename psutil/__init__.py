59c50ff4a69d94683a8d996f24e9a3cdb4566b9b
Giampaolo Rodola <g.rodola@gmail.com>
2015-08-02 20:04:36 +0200

    maybe we can run OSX/travis

--- psutil/__init__.py
+++ psutil/__init__.py
@@ -486,8 +486,8 @@ class Process(object):
         try:
             # Checking if PID is alive is not enough as the PID might
             # have been reused by another process: we also want to
-            # check process identity.
-            # Process identity / uniqueness over time is greanted by
+            # verify process identity.
+            # Process identity / uniqueness over time is guaranteed by
             # (PID + creation time) and that is verified in __eq__.
             return self == Process(self.pid)
         except NoSuchProcess:
