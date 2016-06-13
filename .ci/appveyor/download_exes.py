4c067a82075d503e5e287aa9adb23e0a048a8f64
Giampaolo Rodola <g.rodola@gmail.com>
2016-06-13 03:22:05 +0200

    appveyor download script: check the num of download files and print a warning if it's < than expected

--- .ci/appveyor/download_exes.py
+++ .ci/appveyor/download_exes.py
@@ -25,6 +25,37 @@ from concurrent.futures import ThreadPoolExecutor
 
 
 BASE_URL = 'https://ci.appveyor.com/api'
+PY_VERSIONS = ['2.7', '3.3', '3.4', '3.5']
+
+
+def term_supports_colors(file=sys.stdout):
+    try:
+        import curses
+        assert file.isatty()
+        curses.setupterm()
+        assert curses.tigetnum("colors") > 0
+    except Exception:
+        return False
+    else:
+        return True
+
+
+if term_supports_colors():
+    def hilite(s, ok=True, bold=False):
+        """Return an highlighted version of 'string'."""
+        attr = []
+        if ok is None:  # no color
+            pass
+        elif ok:   # green
+            attr.append('32')
+        else:   # red
+            attr.append('31')
+        if bold:
+            attr.append('1')
+        return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), s)
+else:
+    def hilite(s, *a, **k):
+        return s
 
 
 def safe_makedirs(path):
@@ -48,15 +79,16 @@ def safe_rmtree(path):
 
 
 def download_file(url):
-    local_filename = url.split('/')[-1]
-    local_filename = os.path.join('dist', local_filename)
-    print(local_filename)
+    local_fname = url.split('/')[-1]
+    local_fname = os.path.join('dist', local_fname)
+    print(local_fname)
     safe_makedirs('dist')
     r = requests.get(url, stream=True)
-    with open(local_filename, 'wb') as f:
+    with open(local_fname, 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk:    # filter out keep-alive new chunks
                 f.write(chunk)
+    return local_fname
 
 
 def get_file_urls(options):
@@ -72,7 +104,6 @@ def get_file_urls(options):
         data = data.json()
         for item in data:
             file_url = job_url + '/' + item['fileName']
-            print(file_url)
             urls.append(file_url)
     if not urls:
         sys.exit("no artifacts found")
@@ -81,10 +112,18 @@ def get_file_urls(options):
 
 
 def main(options):
+    files = []
     safe_rmtree('dist')
     with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as e:
         for url in get_file_urls(options):
-            e.submit(download_file, url)
+            fut = e.submit(download_file, url)
+            files.append(fut.result())
+    # 2 exes (32 and 64 bit) and 2 wheels (32 and 64 bit) for each ver.
+    expected = len(PY_VERSIONS) * 4
+    got = len(files)
+    if expected != got:
+        print(hilite("expected %s files, got %s" % (expected, got), ok=False),
+              file=sys.stderr)
 
 
 if __name__ == '__main__':
