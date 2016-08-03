db2bb6dcdd5379da02c54e2bbca849d039c66254
Giampaolo Rodola <g.rodola@gmail.com>
2016-08-03 20:26:57 +0200

    enhance benchmark script

--- scripts/internal/bench_oneshot.py
+++ scripts/internal/bench_oneshot.py
@@ -16,6 +16,8 @@ import time
 import psutil
 
 
+ITERATIONS = 1000
+
 # The list of Process methods which gets collected in one shot and
 # as such get advantage of the speedup.
 if psutil.LINUX:
@@ -38,23 +40,31 @@ def call(funs):
 def main():
     p = psutil.Process()
     funs = collect(p)
-
     print("%s methods involved on platform %r" % (len(names), sys.platform))
 
+    # first "normal" run
     t = time.time()
-    for x in range(1000):
+    for x in range(ITERATIONS):
         call(funs)
     elapsed1 = time.time() - t
-    print("normal:  %.3f" % elapsed1)
+    print("normal:  %.3f secs" % elapsed1)
 
+    # "one shot" run
     t = time.time()
-    for x in range(1000):
+    for x in range(ITERATIONS):
         with p.oneshot():
+            time.sleep(.0001)
             call(funs)
     elapsed2 = time.time() - t
-    print("oneshot: %.3f" % elapsed2)
+    print("oneshot: %.3f secs" % elapsed2)
 
-    print("speedup: +%.2fx" % (elapsed1 / elapsed2))
+    # done
+    if elapsed2 < elapsed1:
+        print("speedup: +%.2fx" % (elapsed1 / elapsed2))
+    elif elapsed2 > elapsed1:
+        print("slowdown: -%.2fx" % (elapsed1 / elapsed2))
+    else:
+        print("same speed")
 
 
 main()
