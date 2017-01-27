2cd73cafee4548e2b73d0797ca8a1f7b06ccddae
Giampaolo Rodola <g.rodola@gmail.com>
2017-01-27 05:12:07 +0100

    BSD: expose sensors_batter() for FreeBSD only

--- psutil/_psbsd.py
+++ psutil/_psbsd.py
@@ -400,16 +400,18 @@ def net_connections(kind):
 # =====================================================================
 
 
-def sensors_battery():
-    percent, minsleft, power_plugged = cext.sensors_battery()
-    power_plugged = power_plugged == 1
-    if power_plugged:
-        secsleft = _common.POWER_TIME_UNLIMITED
-    elif minsleft == -1:
-        secsleft = _common.POWER_TIME_UNKNOWN
-    else:
-        secsleft = minsleft * 60
-    return _common.sbattery(percent, secsleft, power_plugged)
+if FREEBSD:
+
+    def sensors_battery():
+        percent, minsleft, power_plugged = cext.sensors_battery()
+        power_plugged = power_plugged == 1
+        if power_plugged:
+            secsleft = _common.POWER_TIME_UNLIMITED
+        elif minsleft == -1:
+            secsleft = _common.POWER_TIME_UNKNOWN
+        else:
+            secsleft = minsleft * 60
+        return _common.sbattery(percent, secsleft, power_plugged)
 
 
 # =====================================================================
