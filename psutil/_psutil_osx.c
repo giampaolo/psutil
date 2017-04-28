8f7ac93eec042799e6f866cc8b816dab3ebf9ac6
Giampaolo Rodola <g.rodola@gmail.com>
2017-04-28 07:47:12 +0200

    #1029: fix encoding error for proc.econnections('unix') on OSX

--- psutil/_psutil_osx.c
+++ psutil/_psutil_osx.c
@@ -1350,12 +1350,34 @@ psutil_proc_connections(PyObject *self, PyObject *args) {
                 Py_DECREF(py_tuple);
             }
             else if (family == AF_UNIX) {
+                // decode laddr
+                #if PY_MAJOR_VERSION >= 3
+                    py_laddr = PyUnicode_DecodeFSDefault(
+                        si.psi.soi_proto.pri_un.unsi_addr.ua_sun.sun_path);
+                #else
+                    py_laddr = Py_BuildValue("s",
+                        si.psi.soi_proto.pri_un.unsi_addr.ua_sun.sun_path);
+                #endif
+                    if (!py_laddr)
+                        goto error;
+
+                // decode raddr
+                #if PY_MAJOR_VERSION >= 3
+                    py_raddr = PyUnicode_DecodeFSDefault(
+                        si.psi.soi_proto.pri_un.unsi_caddr.ua_sun.sun_path);
+                #else
+                    py_raddr = Py_BuildValue("s",
+                        si.psi.soi_proto.pri_un.unsi_caddr.ua_sun.sun_path);
+                #endif
+                    if (!py_raddr)
+                        goto error;
+
                 // construct the python list
                 py_tuple = Py_BuildValue(
-                    "(iiissi)",
+                    "(iiiOOi)",
                     fd, family, type,
-                    si.psi.soi_proto.pri_un.unsi_addr.ua_sun.sun_path,
-                    si.psi.soi_proto.pri_un.unsi_caddr.ua_sun.sun_path,
+                    py_laddr,
+                    py_raddr,
                     PSUTIL_CONN_NONE);
                 if (!py_tuple)
                     goto error;
