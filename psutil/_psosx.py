75af5260cb783a538d19db2a937b67305e7051e9
amanusk <amanusk@protonmail.com>
2018-06-22 15:11:38 +0300

    Add additional SMC keys, divided into groups
    Add a lot of additional SMC keys
    Remove get_smc_temperatures utility funcion

--- psutil/_psosx.py
+++ psutil/_psosx.py
@@ -213,11 +213,13 @@ def disk_partitions(all=False):
 # =====================================================================
 
 
-def get_smc_temperatures(smc_keys, iter_num, iteratable):
-    """ Receives a dictionary of SMC keys, and a number of possible keys.
-    If keys can be iterated, replaces the expected %i in the key.
-    Returns a list of all smc_get_temperature results higher than 0.
+def sensors_temperatures():
+    """Return CPU temperatures including key name and temperature.
+
     References:
+
+    https://stackoverflow.com/questions/28568775/
+        description-for-apples-smc-keys/31033665#31033665
 
     https://github.com/Chris911/iStats/blob/
         09b159f85a9481b59f347a37259f6d272f65cc05/lib/iStats/smc.rb
@@ -225,75 +227,119 @@ def get_smc_temperatures(smc_keys, iter_num, iteratable):
     http://web.archive.org/web/20140714090133/http://www.parhelia.ch:80/
         blog/statics/k3_keys.html
     """
-    ret = list()
-    if iteratable:
-        for idx in range(iter_num):
-            for key, label in smc_keys.items():
-                result = cext.smc_get_temperature(key % idx)
-                result = round(result, 1)
-                if result <= 0:
-                    continue
-                ret.append((label % idx, result, None, None))
-    else:
-        for key, label in smc_keys.items():
-            result = cext.smc_get_temperature(key)
-            result = round(result, 1)
-            if result <= 0:
-                continue
-            ret.append((label, result, None, None))
-
-    return ret
-
-
-def sensors_temperatures():
-    """Return CPU temperatures including key name and temperature.  """
     ret = collections.defaultdict(list)
-    # Handle CPU cores
-    num_cpus = cext.cpu_count_phys()
-    core_temps = {
-        "TC%iC": "Core %i",
-    }
-    if num_cpus:
-        results = get_smc_temperatures(core_temps, num_cpus + 1, True)
-        ret["CPU"] = ret["CPU"] + results
+    temperatures = (
+        # group, key, label
 
-    # Handle other CPU sensors
-    cpu_temps = {
-        "TC%iF": "CPU TC%iF",
-        "TC%iG": "CPU TC%iG",
-        "TC%iP": "CPU %i Proximity",
-    }
-    num_packages = cext.cpu_count_packages()
-    if num_packages:
-        results = get_smc_temperatures(cpu_temps, num_packages, True)
-        ret["CPU"] = ret["CPU"] + results
+        # --- CPU
+        ("CPU", "TCXC", "PECI CPU"),
+        ("CPU", "TCXc", "PECI CPU"),
+        ("CPU", "TC0P", "CPU 1 Proximity"),
+        ("CPU", "TC0H", "CPU 1 Heatsink"),
+        ("CPU", "TC0D", "CPU 1 Package"),
+        ("CPU", "TC0E", "CPU 1"),
+        ("CPU", "TC0F", "CPU 1"),
+        ("CPU", "TC1C", "CPU Core 1"),
+        ("CPU", "TC2C", "CPU Core 2"),
+        ("CPU", "TC3C", "CPU Core 3"),
+        ("CPU", "TC4C", "CPU Core 4"),
+        ("CPU", "TC5C", "CPU Core 5"),
+        ("CPU", "TC6C", "CPU Core 6"),
+        ("CPU", "TC7C", "CPU Core 7"),
+        ("CPU", "TC8C", "CPU Core 8"),
+        ("CPU", "TCAH", "CPU 1 Heatsink Alt."),
+        ("CPU", "TCAD", "CPU 1 Package Alt."),
+        ("CPU", "TC1P", "CPU 2 Proximity"),
+        ("CPU", "TC1H", "CPU 2 Heatsink"),
+        ("CPU", "TC1D", "CPU 2 Package"),
+        ("CPU", "TC1E", "CPU 2"),
+        ("CPU", "TC1F", "CPU 2"),
+        ("CPU", "TCBH", "CPU 2 Heatsink Alt."),
+        ("CPU", "TCBD", "CPU 2 Package Alt."),
 
-    # Handle Battery Temperature
-    battery_temps = {
-        "TB%iT": "Battery %i",
-    }
-    # 2 is the highest number of batteries recoreded in iStats
-    results = get_smc_temperatures(battery_temps, 3, True)
-    ret["Battery"] = ret["Battery"] + results
+        ("CPU", "TCSC", "PECI SA"),
+        ("CPU", "TCSc", "PECI SA"),
+        ("CPU", "TCSA", "PECI SA"),
 
-    # Handle HDD temperatures
-    hdd_temps = {
-        "TH%iP": "HDD %i Proximity",
-        "TH%ia": "HDD %i Unknown",
-    }
-    # 2 is a logically normal number of HDDs
-    results = get_smc_temperatures(hdd_temps, 2, True)
-    ret["HDD"] = ret["HDD"] + results
+        # --- GPU
+        ("GPU", "TCGC", "PECI GPU"),
+        ("GPU", "TCGc", "PECI GPU"),
+        ("GPU", "TG0P", "GPU Proximity"),
+        ("GPU", "TG0D", "GPU Die"),
+        ("GPU", "TG1D", "GPU Die"),
+        ("GPU", "TG0H", "GPU Heatsink"),
+        ("GPU", "TG1H", "GPU Heatsink"),
 
-    # Handle GPU temperature
-    gpu_temps = {
-        "TG%iH": "GPU %i Heatsink",
-        "TG%iP": "GPU %i Proximity",
-        "TG%iD": "GPU %i Die",
-    }
-    # Assume 2 possible GPUs
-    results = get_smc_temperatures(gpu_temps, 2, True)
-    ret["GPU"] = ret["GPU"] + results
+        # --- Memory
+        ("Memory", "Ts0S", "Memory Proximity"),
+        ("Memory", "TM0P", "Mem Bank A1"),
+        ("Memory", "TM1P", "Mem Bank A2"),
+        ("Memory", "TM8P", "Mem Bank B1"),
+        ("Memory", "TM9P", "Mem Bank B2"),
+        ("Memory", "TM0S", "Mem Module A1"),
+        ("Memory", "TM1S", "Mem Module A2"),
+        ("Memory", "TM8S", "Mem Module B1"),
+        ("Memory", "TM9S", "Mem Module B2"),
+
+        # --- HDD
+        ("HDD", "TH0P", "HDD Bay 1"),
+        ("HDD", "TH1P", "HDD Bay 2"),
+        ("HDD", "TH2P", "HDD Bay 3"),
+        ("HDD", "TH3P", "HDD Bay 4"),
+
+        # --- Battery
+        ("Battery", "TB0T", "Battery TS_MAX"),
+        ("Battery", "TB1T", "Battery 1"),
+        ("Battery", "TB2T", "Battery 2"),
+        ("Battery", "TB3T", "Battery"),
+
+        # --- Others
+        ("Others", "TN0D", "Northbridge Die"),
+        ("Others", "TN0P", "Northbridge Proximity 1"),
+        ("Others", "TN1P", "Northbridge Proximity 2"),
+        ("Others", "TN0C", "MCH Die"),
+        ("Others", "TN0H", "MCH Heatsink"),
+        ("Others", "TP0D", "PCH Die"),
+        ("Others", "TPCD", "PCH Die"),
+        ("Others", "TP0P", "PCH Proximity"),
+
+        ("Others", "TA0P", "Airflow 1"),
+        ("Others", "TA1P", "Airflow 2"),
+        ("Others", "Th0H", "Heatpipe 1"),
+        ("Others", "Th1H", "Heatpipe 2"),
+        ("Others", "Th2H", "Heatpipe 3"),
+
+        ("Others", "Tm0P", "Mainboard Proximity"),
+        ("Others", "Tp0P", "Powerboard Proximity"),
+        ("Others", "Ts0P", "Palm Rest"),
+        ("Others", "Tb0P", "BLC Proximity"),
+
+        ("Others", "TL0P", "LCD Proximity"),
+        ("Others", "TW0P", "Airport Proximity"),
+        ("Others", "TO0P", "Optical Drive"),
+
+        ("Others", "Tp0P", "Power Supply 1"),
+        ("Others", "Tp0C", "Power Supply 1 Alt."),
+        ("Others", "Tp1P", "Power Supply 2"),
+        ("Others", "Tp1C", "Power Supply 2 Alt."),
+        ("Others", "Tp2P", "Power Supply 3"),
+        ("Others", "Tp3P", "Power Supply 4"),
+        ("Others", "Tp4P", "Power Supply 5"),
+        ("Others", "Tp5P", "Power Supply 6"),
+
+        ("Others", "TS0C", "Expansion Slots"),
+        ("Others", "TA0S", "PCI Slot 1 Pos 1"),
+        ("Others", "TA1S", "PCI Slot 1 Pos 2"),
+        ("Others", "TA2S", "PCI Slot 2 Pos 1"),
+        ("Others", "TA3S", "PCI Slot 2 Pos 2"),
+    )
+
+    for group, key, label in temperatures:
+        result = cext.smc_get_temperature(key)
+        result = round(result, 1)
+        if result <= 0:
+            continue
+        ret[group].append((label, result, None, None))
 
     return dict(ret)
 
