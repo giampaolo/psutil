4b71736b72b6b68a81dfb22d2df431c57ef1fde1
Giampaolo Rodola <g.rodola@gmail.com>
2018-08-13 22:02:29 +0200

    setup.py: add py 3.7

--- setup.py
+++ setup.py
@@ -322,6 +322,7 @@ def main():
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: Implementation :: CPython',
             'Programming Language :: Python :: Implementation :: PyPy',
             'Programming Language :: Python',
