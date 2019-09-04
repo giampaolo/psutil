e9144d1fef51be78376e55d019e55fa1fa137109
itachee <dev.itachee@gmail.com>
2019-09-04 00:47:34 -0700

    added file filler

--- psutil/arch/osx/specific.c
+++ psutil/arch/osx/specific.c
@@ -1,21 +1,13 @@
 /*
- * Apple System Management Control (SMC) Tool
- * Copyright (C) 2006 devnull 
+ * Copyright (c) 2009, itachee. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * OS X CPU temp reading using SMC.
+ * Can be extended for more sensors in future
  */
+
+
 
 #ifndef __SMC_H__
 #define __SMC_H__
--- psutil/arch/osx/specific.h
+++ psutil/arch/osx/specific.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2009, itachee. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ *
+ * OS X CPU temp reading using SMC.
+ * Can be extended for more sensors in future
+ */
+
 #ifndef __OSX_SPECIFIC_H
 #define __OSX_SPECIFIC_H
 #include <Python.h>
