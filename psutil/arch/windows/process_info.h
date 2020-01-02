65cd0892bb55362dd59ccb31eed5756f640f8df4
Giampaolo Rodola <g.rodola@gmail.com>
2020-01-02 12:40:36 -0800

    remove unnucessary includes

--- psutil/arch/windows/process_info.h
+++ psutil/arch/windows/process_info.h
@@ -4,21 +4,10 @@
  * found in the LICENSE file.
  */
 
-#if !defined(__PROCESS_INFO_H)
-#define __PROCESS_INFO_H
-
 #include <Python.h>
-#include <windows.h>
-#include "security.h"
-#include "ntextapi.h"
-
-#define HANDLE_TO_PYNUM(handle) PyLong_FromUnsignedLong((unsigned long) handle)
-#define PYNUM_TO_HANDLE(obj) ((HANDLE)PyLong_AsUnsignedLong(obj))
 
 int psutil_get_proc_info(DWORD pid, PSYSTEM_PROCESS_INFORMATION *retProcess,
                          PVOID *retBuffer);
 PyObject* psutil_get_cmdline(long pid, int use_peb);
 PyObject* psutil_get_cwd(long pid);
 PyObject* psutil_get_environ(long pid);
-
-#endif
