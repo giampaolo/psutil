eb5ce8033263aafe37c35a875c5bb57c8f538353
Giampaolo Rodola <g.rodola@gmail.com>
2020-02-20 16:24:43 +0100

    refactor CI scripts

--- .ci/travis/run.sh
+++ .ci/travis/run.sh
@@ -13,26 +13,26 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate psutil
 fi
 
-# install psutil
-make clean
-python setup.py build
-python setup.py develop
+make install
+
+echo "Testing on Python $PYVER"
+$PYTHON -m pip freeze
 
 # run tests (with coverage)
 if [[ $PYVER == '2.7' ]] && [[ "$(uname -s)" != 'Darwin' ]]; then
     PSUTIL_TESTING=1 python -Wa -m coverage run psutil/tests/runner.py
 else
-    PSUTIL_TESTING=1 python -Wa psutil/tests/runner.py
+    make test PYTHON=python
 fi
 
 if [ "$PYVER" == "2.7" ] || [ "$PYVER" == "3.6" ]; then
     # run mem leaks test
-    PSUTIL_TESTING=1 python -Wa psutil/tests/test_memory_leaks.py
+    make test-memleaks PYTHON=python
     # run linter (on Linux only)
     if [[ "$(uname -s)" != 'Darwin' ]]; then
         make lint PYTHON=python
     fi
 fi
 
-PSUTIL_TESTING=1 python -Wa scripts/internal/print_access_denied.py
-PSUTIL_TESTING=1 python -Wa scripts/internal/print_api_speed.py
+make print-access-denied PYTHON=python
+make print-api-speed PYTHON=python
