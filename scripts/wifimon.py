3e3dea5e208da05730107515211a51348c20a761
Giampaolo Rodola <g.rodola@gmail.com>
2020-10-07 01:49:41 +0200

    make it nicer

--- scripts/wifimon.py
+++ scripts/wifimon.py
@@ -26,6 +26,7 @@ def setup():
     for i in range(0, curses.COLORS):
         curses.init_pair(i + 1, i, -1)
     curses.endwin()
+    win.nodelay(1)
 
 
 win = curses.initscr()
@@ -53,7 +54,7 @@ colors_map = dict(
 )
 
 
-def printl(line, color=None, bold=False):
+def printl(line, color=None, bold=False, underline=False):
     """A thin wrapper around curses's addstr()."""
     global lineno
     try:
@@ -62,6 +63,8 @@ def printl(line, color=None, bold=False):
             flags |= curses.color_pair(colors_map[color])
         if bold:
             flags |= curses.A_BOLD
+        if underline:
+            flags |= curses.A_UNDERLINE
         win.addstr(lineno, 0, line, flags)
     except curses.error:
         lineno = 0
@@ -72,7 +75,7 @@ def printl(line, color=None, bold=False):
 
 
 def printl_title(line):
-    printl(line, color="blue", bold=True)
+    printl(line, color="blue", bold=True, underline=True)
 
 
 def get_dashes(perc):
@@ -83,47 +86,49 @@ def get_dashes(perc):
 
 def refresh_window():
     """Print results on screen by using curses."""
-    global lineno
-
     for ifname, info in psutil.wifi_ifaces().items():
-        printl_title("INTERFACE")
-        printl("  %s (%s), SSID: %s" % (ifname, info.proto, info.essid))
+        printl_title("Interface")
+        printl("    %s (%s)" % (ifname, info.proto))
+        printl("    SSID: %s" % (info.essid))
         printl("")
 
-        printl_title("LEVELS")
-        printl("  Link quality: %s%%" % info.quality_percent)
+        printl_title("Levels")
+        printl("    Link quality: %s%%" % info.quality_percent)
         dashes, empty_dashes = get_dashes(info.quality_percent)
-        line = "  [%s%s]" % (dashes, empty_dashes)
-        printl(line, color="green" if info.quality_percent >= 50 else "red")
+        line = "    [%s%s]" % (dashes, empty_dashes)
+        printl(line, color="green" if info.quality_percent >= 50 else "red",
+               bold=True)
+        printl("")
 
-        printl("  Signal level: %s dBm (%s%%)" % (
+        printl("    Signal level: %s dBm (%s%%)" % (
             info.signal, info.signal_percent))
         dashes, empty_dashes = get_dashes(info.signal_percent)
-        line = "  [%s%s]" % (dashes, empty_dashes)
-        printl(line, color="green" if info.signal_percent >= 50 else "red")
+        line = "    [%s%s]" % (dashes, empty_dashes)
+        printl(line, color="green" if info.signal_percent >= 50 else "red",
+               bold=True)
         printl("")
 
-        printl_title("STATISTICS")
+        printl_title("Statistics")
         ioc = psutil.net_io_counters(pernic=True)[ifname]
-        printl("  RX (recv): %6s (%6s)" % (
+        printl("    RX (recv): %6s (%6s)" % (
             bytes2human(ioc.bytes_recv), '{0:,}'.format(ioc.bytes_recv)))
-        printl("  TX (sent): %6s (%6s)" % (
+        printl("    TX (sent): %6s (%6s)" % (
             bytes2human(ioc.bytes_sent), '{0:,}'.format(ioc.bytes_sent)))
         printl("")
 
-        printl_title("INFO")
-        printl("  Connected to: %s" % (info.bssid))
-        printl("  Mode: %s" % (info.mode))
-        printl("  Freq: %s MHz" % (info.freq))
-        printl("  TX-Power: %s" % (info.txpower))
-        printl("  Power save: %s" % ("on" if info.power_save else "off"))
-        printl("  Beacons: %s, Nwid: %s, Crypt: %s" % (
+        printl_title("Info")
+        printl("    Connected to: %s" % (info.bssid))
+        printl("    Mode: %s" % (info.mode))
+        printl("    Freq: %s MHz" % (info.freq))
+        printl("    TX-Power: %s" % (info.txpower))
+        printl("    Power save: %s" % ("on" if info.power_save else "off"))
+        printl("    Beacons: %s, Nwid: %s, Crypt: %s" % (
             info.beacons, info.discard_nwid, info.discard_crypt))
-        printl("  Frag: %s, Retry: %s, Misc: %s" % (
+        printl("    Frag: %s, Retry: %s, Misc: %s" % (
             info.discard_frag, info.discard_retry, info.discard_misc))
         printl("")
 
-        printl_title("ADDRESSES (%s)" % ifname)
+        printl_title("Addresses (%s)" % ifname)
         addrs = psutil.net_if_addrs()[ifname]
         for addr in addrs:
             proto = af_map.get(addr.family, addr.family)
@@ -131,17 +136,20 @@ def refresh_window():
 
     printl("")
     win.refresh()
-    lineno = 0
 
 
 def main():
+    global lineno
     if not hasattr(psutil, "wifi_ifaces"):
         sys.exit('platform not supported')
     if not psutil.wifi_ifaces():
         sys.exit('no Wi-Fi interfaces installed')
     try:
         while True:
+            if win.getch() == ord('q'):
+                break
             refresh_window()
+            lineno = 0
             time.sleep(INTERVAL)
     except (KeyboardInterrupt, SystemExit):
         pass
