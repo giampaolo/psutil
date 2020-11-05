957e4d86c54888b279bdcc977c4dd97543dfc1e1
Giampaolo Rodola <g.rodola@gmail.com>
2020-11-05 22:17:33 +0100

    md5sum

---
+++ scripts/internal/md5sum.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Copyright (c) 2009 Giampaolo Rodola'. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A clone of md5sum utility on UNIX. Prints MD5 checksums of file(s)."""
+
+import os
+import hashlib
+import sys
+
+
+def md5(file):
+    h = hashlib.new("md5")
+    with open(file, 'rb') as f:
+        h.update(f.read())
+        return h.hexdigest()
+
+
+def main():
+    files = sys.argv[1:]
+    if not files:
+        sys.exit('provide an argument')
+    for file in files:
+        print("%s %s" % (os.path.basename(file), md5(file)))
+
+
+if __name__ == '__main__':
+    main()
