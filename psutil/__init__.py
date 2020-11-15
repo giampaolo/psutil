f8bae4e4de77fe08936ce71a4857acecaf5d1117
Giampaolo Rodola <g.rodola@gmail.com>
2020-11-15 19:33:17 +0100

    bump up version

--- psutil/__init__.py
+++ psutil/__init__.py
@@ -209,7 +209,7 @@ if hasattr(_psplatform.Process, "rlimit"):
 AF_LINK = _psplatform.AF_LINK
 
 __author__ = "Giampaolo Rodola'"
-__version__ = "5.7.4"
+__version__ = "5.8.0"
 version_info = tuple([int(num) for num in __version__.split('.')])
 
 _timer = getattr(time, 'monotonic', time.time)
