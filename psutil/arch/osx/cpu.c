a1e3ba60383a1fc0fe98a519fcafe48558d45cdf
Giampaolo Rodola <g.rodola@gmail.com>
2020-12-20 21:04:12 +0100

    macOS, get CPU extended features
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>

--- psutil/arch/osx/cpu.c
+++ psutil/arch/osx/cpu.c
@@ -90,32 +90,86 @@ psutil_cpu_vendor() {
 }
 
 
+// Returns a string like this:
+// 'fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov ...'
 static PyObject *
 psutil_cpu_features() {
-    size_t len;
+    size_t len1;
+    size_t len2;
+    char *buf1 = NULL;
+    char *buf2 = NULL;
+    char *buf3 = NULL;
     int i;
-    char *buffer;
     PyObject *py_str = NULL;
 
-    if (sysctlbyname("machdep.cpu.features", NULL, &len, NULL, 0) != 0) {
+    // There are standard and extended features (both strings). First,
+    // get the size for both.
+    if (sysctlbyname("machdep.cpu.features", NULL, &len1, NULL, 0) != 0) {
         psutil_debug("sysctlbyname('machdep.cpu.features') failed (ignored)");
         Py_RETURN_NONE;
     }
 
-    buffer = malloc(len);
-    if (sysctlbyname("machdep.cpu.features", buffer, &len, NULL, 0) != 0) {
-        free(buffer);
-        psutil_debug("sysctlbyname('machdep.cpu.features') failed (ignored)");
+    if (sysctlbyname("machdep.cpu.extfeatures", NULL, &len2, NULL, 0) != 0) {
+        psutil_debug("sysctlbyname('machdep.cpu.extfeatures') failed (ignored)");
         Py_RETURN_NONE;
     }
 
-    // lowercase str
-    for (i = 0; buffer[i]; i++)
-        buffer[i] = tolower(buffer[i]);
+    // Now we get the real values; we need 2 mallocs.
 
-    py_str = Py_BuildValue("s", buffer);
-    free(buffer);
+    // ...standard
+    buf1 = malloc(len1);
+    if (buf1 == NULL) {
+        PyErr_NoMemory();
+        goto error;
+    }
+    if (sysctlbyname("machdep.cpu.features", buf1, &len1, NULL, 0) != 0) {
+        psutil_debug("sysctlbyname('machdep.cpu.features') failed (ignored)");
+        goto error;
+    }
+
+    // ...extended
+    buf2 = malloc(len2);
+    if (buf2 == NULL) {
+        PyErr_NoMemory();
+        goto error;
+    }
+    if (sysctlbyname("machdep.cpu.extfeatures", buf2, &len2, NULL, 0) != 0) {
+        psutil_debug("sysctlbyname('machdep.cpu.extfeatures') failed (ignored)");
+        goto error;
+    }
+
+    // Lower case both strings (mimic Linux lscpu).
+    for (i = 0; buf1[i]; i++)
+        buf1[i] = tolower(buf1[i]);
+    for (i = 0; buf2[i]; i++)
+        buf2[i] = tolower(buf2[i]);
+
+    // Make space for both in a new buffer and join them (+1 is for the
+    // null terminator).
+    buf3 = malloc(len1 + len2 + 1);
+    if (buf3 == NULL) {
+        PyErr_NoMemory();
+        goto error;
+    }
+    sprintf(buf3, "%s %s", buf1, buf2);
+
+    // Return.
+    py_str = Py_BuildValue("s", buf3);
+    free(buf1);
+    free(buf2);
+    free(buf3);
     return py_str;
+
+error:
+    if (buf1 != NULL)
+        free(buf1);
+    if (buf2 != NULL)
+        free(buf2);
+    if (buf3 != NULL)
+        free(buf3);
+    if (PyErr_Occurred())  // malloc failed
+        return NULL;
+    Py_RETURN_NONE;
 }
 
 
