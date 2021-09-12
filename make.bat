ad945dddbc374a7d3d7abde5079f2bb1b31653f2
Robert Nowotny <bitranox@gmail.com>
2021-09-12 16:54:20 +0200

    Update make.bat
    Support Blank in env PYTHON
    report if executable not found

--- make.bat
+++ make.bat
@@ -1,23 +1,24 @@
 @echo off
 
-rem ==========================================================================
-rem Shortcuts for various tasks, emulating UNIX "make" on Windows.
-rem It is primarly intended as a shortcut for compiling / installing
-rem psutil ("make.bat build", "make.bat install") and running tests
-rem ("make.bat test").
-rem
-rem This script is modeled after my Windows installation which uses:
-rem - Visual studio 2008 for Python 2.6, 2.7
-rem - Visual studio 2010 for Python 3.4+
-rem ...therefore it might not work on your Windows installation.
-rem
-rem By default C:\Python27\python.exe is used.
-rem To compile for a specific Python version run:
-rem     set PYTHON=C:\Python34\python.exe & make.bat build
-rem
-rem To use a different test script:
-rem      set PYTHON=C:\Python34\python.exe & set TSCRIPT=foo.py & make.bat test
-rem ==========================================================================
+:: ==========================================================================
+:: Shortcuts for various tasks, emulating UNIX "make" on Windows.
+:: It is primary intended as a shortcut for compiling / installing
+:: psutil ("make.bat build", "make.bat install") and running tests
+:: ("make.bat test").
+::
+:: This script is modeled after my Windows installation which uses:
+:: - Visual studio 2008 for Python 2.6, 2.7
+:: - Visual studio 20?? for Python 3.8+
+:: ...therefore it might not work on your Windows installation.
+::
+:: By default C:\Python27\python.exe is used.
+:: To compile for a specific Python version run:
+::     set PYTHON=C:\Python34\python.exe & make.bat build
+::
+:: To use a different test script:
+::      set PYTHON=C:\Python38-64\python.exe & set TSCRIPT=foo.py & make.bat test
+:: ==========================================================================
+
 
 if "%PYTHON%" == "" (
     if exist "C:\Python38-64\python.exe" (
@@ -27,11 +28,15 @@ if "%PYTHON%" == "" (
     )
 )
 
+if NOT exist "%PYTHON%" (
+    echo Python Interpreter "%PYTHON%" not found, You need to set the PYTHON environment variable accordingly
+    pause
+	exit /b 1
+    )
+
+
 if "%TSCRIPT%" == "" (
     set TSCRIPT=psutil\tests\runner.py
 )
 
-rem Needed to locate the .pypirc file and upload exes on PyPI.
-set HOME=%USERPROFILE%
-
-%PYTHON% scripts\internal\winmake.py %1 %2 %3 %4 %5 %6
+"%PYTHON%" scripts\internal\winmake.py %1 %2 %3 %4 %5 %6
