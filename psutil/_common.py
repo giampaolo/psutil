86c5e90faddae728d3600867cc8dff4f4c24a3e3
Saeed Rasooli <saeed.gnu@gmail.com>
2021-10-11 21:35:19 +0330

    add support for MidnightBSD
    Signed-off-by: Saeed Rasooli <saeed.gnu@gmail.com>

--- psutil/_common.py
+++ psutil/_common.py
@@ -83,7 +83,7 @@ WINDOWS = os.name == "nt"
 LINUX = sys.platform.startswith("linux")
 MACOS = sys.platform.startswith("darwin")
 OSX = MACOS  # deprecated alias
-FREEBSD = sys.platform.startswith("freebsd")
+FREEBSD = sys.platform.startswith(("freebsd", "midnightbsd"))
 OPENBSD = sys.platform.startswith("openbsd")
 NETBSD = sys.platform.startswith("netbsd")
 BSD = FREEBSD or OPENBSD or NETBSD
