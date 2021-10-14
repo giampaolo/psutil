c2303603e82ee0faec9cbd454960b834e185e46d
Giampaolo Rodola <g.rodola@gmail.com>
2021-10-14 17:02:08 +0200

    catch race condition
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>

--- psutil/_pslinux.py
+++ psutil/_pslinux.py
@@ -1228,11 +1228,16 @@ class RootFsDeviceFinder:
         needle = "%d:%d" % (self.major, self.minor)
         files = glob.glob("/sys/class/block/*/dev")
         for file in files:
-            with open_text(file) as f:
-                data = f.read().strip()
-                if data == needle:
-                    name = os.path.basename(os.path.dirname(file))
-                    return "/dev/%s" % name
+            try:
+                f = open_text(file)
+            except FileNotFoundError:  # race condition
+                continue
+            else:
+                with f:
+                    data = f.read().strip()
+                    if data == needle:
+                        name = os.path.basename(os.path.dirname(file))
+                        return "/dev/%s" % name
 
     def find(self):
         path = None
