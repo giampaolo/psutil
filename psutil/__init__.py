fcd818c279b03d7f405c254b200aa1980103601d
Giampaolo Rodola <g.rodola@gmail.com>
2021-12-30 20:52:07 +0100

    make VIRTUAL_ constants part of the global psutil namespace
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>

--- psutil/__init__.py
+++ psutil/__init__.py
@@ -104,6 +104,30 @@ if LINUX:
     from ._pslinux import IOPRIO_CLASS_IDLE  # NOQA
     from ._pslinux import IOPRIO_CLASS_NONE  # NOQA
     from ._pslinux import IOPRIO_CLASS_RT  # NOQA
+    from ._pslinux import VIRTUALIZATION_ACRN  # NOQA
+    from ._pslinux import VIRTUALIZATION_AMAZON  # NOQA
+    from ._pslinux import VIRTUALIZATION_BHYVE  # NOQA
+    from ._pslinux import VIRTUALIZATION_BOCHS  # NOQA
+    from ._pslinux import VIRTUALIZATION_DOCKER  # NOQA
+    from ._pslinux import VIRTUALIZATION_KVM  # NOQA
+    from ._pslinux import VIRTUALIZATION_LXC  # NOQA
+    from ._pslinux import VIRTUALIZATION_LXC_LIBVIRT  # NOQA
+    from ._pslinux import VIRTUALIZATION_MICROSOFT  # NOQA
+    from ._pslinux import VIRTUALIZATION_ORACLE  # NOQA
+    from ._pslinux import VIRTUALIZATION_PARALLELS  # NOQA
+    from ._pslinux import VIRTUALIZATION_PODMAN  # NOQA
+    from ._pslinux import VIRTUALIZATION_POWERVM  # NOQA
+    from ._pslinux import VIRTUALIZATION_PROOT  # NOQA
+    from ._pslinux import VIRTUALIZATION_QEMU  # NOQA
+    from ._pslinux import VIRTUALIZATION_QNX  # NOQA
+    from ._pslinux import VIRTUALIZATION_RKT  # NOQA
+    from ._pslinux import VIRTUALIZATION_SYSTEMD_NSPAWN  # NOQA
+    from ._pslinux import VIRTUALIZATION_UML  # NOQA
+    from ._pslinux import VIRTUALIZATION_VM_OTHER  # NOQA
+    from ._pslinux import VIRTUALIZATION_VMWARE  # NOQA
+    from ._pslinux import VIRTUALIZATION_WSL  # NOQA
+    from ._pslinux import VIRTUALIZATION_XEN  # NOQA
+    from ._pslinux import VIRTUALIZATION_ZVM  # NOQA
 
 elif WINDOWS:
     from . import _pswindows as _psplatform
