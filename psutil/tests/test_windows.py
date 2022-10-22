0b0ad4964286c2a2e7e925d6a1a0b0841d4ee0b9
Daniel Widdis <widdis@gmail.com>
2022-10-22 10:34:40 -0700

    Put unit test back to percent
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- psutil/tests/test_windows.py
+++ psutil/tests/test_windows.py
@@ -175,14 +175,14 @@ class TestSystemAPIs(WindowsTestCase):
         if (psutil.swap_memory().total > 0):
             w = wmi.WMI().Win32_PerfRawData_PerfOS_PagingFile(
                 Name="_Total")[0]
-            # calculate swap usage to integer percent
-            percentSwap = float(w.PercentUsage) / float(w.PercentUsage_Base)
+            # calculate swap usage to percent
+            percentSwap = int(w.PercentUsage) * 100 / int(w.PercentUsage_Base)
             # exact percent may change but should be reasonable
-            # assert within +/- 5% and between 0 and 100
-            self.assertGreaterEqual(psutil.swap_memory().percent, 0.0)
+            # assert within +/- 5% and between 0 and 100%
+            self.assertGreaterEqual(psutil.swap_memory().percent, 0)
             self.assertAlmostEqual(psutil.swap_memory().percent, percentSwap,
-                                   delta=0.05)
-            self.assertLessEqual(psutil.swap_memory().percent, 1.0)
+                                   delta=5)
+            self.assertLessEqual(psutil.swap_memory().percent, 100)
 
     # @unittest.skipIf(wmi is None, "wmi module is not installed")
     # def test__UPTIME(self):
