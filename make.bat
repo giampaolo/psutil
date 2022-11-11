8543b11cc8f9575c28dc748931e590219accf6f9
Giampaolo Rodola <g.rodola@gmail.com>
2022-11-11 03:25:31 -0800

    make.bat: use default 'python' exe; get rid of hard-coded python path

--- make.bat
+++ make.bat
@@ -3,28 +3,26 @@
 rem ==========================================================================
 rem Shortcuts for various tasks, emulating UNIX "make" on Windows.
 rem It is primarily intended as a shortcut for compiling / installing
-rem psutil ("make.bat build", "make.bat install") and running tests
-rem ("make.bat test").
+rem psutil and running tests. E.g.:
+rem
+rem   make build
+rem   make install
+rem   make test
 rem
 rem This script is modeled after my Windows installation which uses:
 rem - Visual studio 2008 for Python 2.7
 rem - Visual studio 2010 for Python 3.4+
 rem ...therefore it might not work on your Windows installation.
 rem
-rem By default C:\Python27\python.exe is used.
 rem To compile for a specific Python version run:
 rem     set PYTHON=C:\Python34\python.exe & make.bat build
 rem
 rem To use a different test script:
-rem      set PYTHON=C:\Python34\python.exe & set TSCRIPT=foo.py & make.bat test
+rem      set TSCRIPT=foo.py & make.bat test
 rem ==========================================================================
 
 if "%PYTHON%" == "" (
-    if exist "C:\Python38-64\python.exe" (
-        set PYTHON=C:\Python38-64\python.exe
-    ) else (
-        set PYTHON=C:\Python27\python.exe
-    )
+    set PYTHON=python
 )
 
 if "%TSCRIPT%" == "" (
