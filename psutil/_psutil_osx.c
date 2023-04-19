12e9327fa0e6fd0af6c7e89bca97d04d5e040e72
Giampaolo Rodola <g.rodola@gmail.com>
2023-04-19 16:51:37 -0700

    OSX, C, refact: move psutil_per_cpu_times() in cpu.c
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>

--- psutil/_psutil_osx.c
+++ psutil/_psutil_osx.c
@@ -12,7 +12,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <utmpx.h>
 #include <sys/sysctl.h>
 #include <libproc.h>
 #include <sys/proc_info.h>
@@ -24,10 +23,7 @@
 #include <mach/mach.h>
 #include <mach/mach_vm.h>
 #include <mach/shared_region.h>
-
 #include <mach-o/loader.h>
-
-#include <CoreFoundation/CoreFoundation.h>
 
 #include "_psutil_common.h"
 #include "_psutil_posix.h"
@@ -471,72 +467,6 @@ psutil_proc_memory_uss(PyObject *self, PyObject *args) {
 
     mach_port_deallocate(mach_task_self(), task);
     return Py_BuildValue("K", private_pages * pagesize);
-}
-
-
-/*
- * Return a Python list of tuple representing per-cpu times
- */
-static PyObject *
-psutil_per_cpu_times(PyObject *self, PyObject *args) {
-    natural_t cpu_count;
-    natural_t i;
-    processor_info_array_t info_array;
-    mach_msg_type_number_t info_count;
-    kern_return_t error;
-    processor_cpu_load_info_data_t *cpu_load_info = NULL;
-    int ret;
-    PyObject *py_retlist = PyList_New(0);
-    PyObject *py_cputime = NULL;
-
-    if (py_retlist == NULL)
-        return NULL;
-
-    mach_port_t host_port = mach_host_self();
-    error = host_processor_info(host_port, PROCESSOR_CPU_LOAD_INFO,
-                                &cpu_count, &info_array, &info_count);
-    if (error != KERN_SUCCESS) {
-        PyErr_Format(
-            PyExc_RuntimeError,
-            "host_processor_info(PROCESSOR_CPU_LOAD_INFO) syscall failed: %s",
-             mach_error_string(error));
-        goto error;
-    }
-    mach_port_deallocate(mach_task_self(), host_port);
-
-    cpu_load_info = (processor_cpu_load_info_data_t *) info_array;
-
-    for (i = 0; i < cpu_count; i++) {
-        py_cputime = Py_BuildValue(
-            "(dddd)",
-            (double)cpu_load_info[i].cpu_ticks[CPU_STATE_USER] / CLK_TCK,
-            (double)cpu_load_info[i].cpu_ticks[CPU_STATE_NICE] / CLK_TCK,
-            (double)cpu_load_info[i].cpu_ticks[CPU_STATE_SYSTEM] / CLK_TCK,
-            (double)cpu_load_info[i].cpu_ticks[CPU_STATE_IDLE] / CLK_TCK
-        );
-        if (!py_cputime)
-            goto error;
-        if (PyList_Append(py_retlist, py_cputime))
-            goto error;
-        Py_CLEAR(py_cputime);
-    }
-
-    ret = vm_deallocate(mach_task_self(), (vm_address_t)info_array,
-                        info_count * sizeof(int));
-    if (ret != KERN_SUCCESS)
-        PyErr_WarnEx(PyExc_RuntimeWarning, "vm_deallocate() failed", 2);
-    return py_retlist;
-
-error:
-    Py_XDECREF(py_cputime);
-    Py_DECREF(py_retlist);
-    if (cpu_load_info != NULL) {
-        ret = vm_deallocate(mach_task_self(), (vm_address_t)info_array,
-                            info_count * sizeof(int));
-        if (ret != KERN_SUCCESS)
-            PyErr_WarnEx(PyExc_RuntimeWarning, "vm_deallocate() failed", 2);
-    }
-    return NULL;
 }
 
 
--- psutil/arch/osx/cpu.h
+++ psutil/arch/osx/cpu.h
@@ -6,8 +6,9 @@
 
 #include <Python.h>
 
-PyObject *psutil_cpu_count_logical(PyObject *self, PyObject *args);
 PyObject *psutil_cpu_count_cores(PyObject *self, PyObject *args);
-PyObject *psutil_cpu_times(PyObject *self, PyObject *args);
+PyObject *psutil_cpu_count_logical(PyObject *self, PyObject *args);
 PyObject *psutil_cpu_freq(PyObject *self, PyObject *args);
 PyObject *psutil_cpu_stats(PyObject *self, PyObject *args);
+PyObject *psutil_cpu_times(PyObject *self, PyObject *args);
+PyObject *psutil_per_cpu_times(PyObject *self, PyObject *args);
