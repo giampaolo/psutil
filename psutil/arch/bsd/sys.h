caa184349c934958b3673c8d3532c607caa2052a
Giampaolo Rodola <g.rodola@gmail.com>
2023-04-19 18:26:01 +0200

    BSD big refact: move proc funcs in new proc.c file

---
+++ psutil/arch/bsd/sys.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2009, Jay Loden, Giampaolo Rodola'. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <Python.h>
+
+PyObject *psutil_boot_time(PyObject *self, PyObject *args);
+PyObject *psutil_users(PyObject *self, PyObject *args);
