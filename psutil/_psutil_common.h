7b34c53514e12ddeec993823ebdb8fdc668c3887
Giampaolo Rodola <g.rodola@gmail.com>
2023-06-08 13:03:53 +0200

    Update _psutil_common.h

--- psutil/_psutil_common.h
+++ psutil/_psutil_common.h
@@ -100,7 +100,6 @@ PyObject* PyErr_SetFromOSErrnoWithSyscall(const char *syscall);
 // ====================================================================
 
 PyObject* psutil_check_pid_range(PyObject *self, PyObject *args);
-
 PyObject* psutil_set_debug(PyObject *self, PyObject *args);
 int psutil_setup(void);
 
