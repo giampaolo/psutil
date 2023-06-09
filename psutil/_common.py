962cb9ad58ca3bc21b20130f435f8d507d931051
Giampaolo Rodola <g.rodola@gmail.com>
2023-06-09 15:37:14 +0200

    bytes2human() is unable to print negative values (#2268)
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>
    
    The bug:
    
    ```
    >>> from psutil._common import bytes2human
    >>> bytes2human(212312454)
    202.5M
    >>> bytes2human(-212312454)
    -212312454
    ```

--- psutil/_common.py
+++ psutil/_common.py
@@ -827,7 +827,7 @@ def bytes2human(n, format="%(value).1f%(symbol)s"):
     for i, s in enumerate(symbols[1:]):
         prefix[s] = 1 << (i + 1) * 10
     for symbol in reversed(symbols[1:]):
-        if n >= prefix[symbol]:
+        if abs(n) >= prefix[symbol]:
             value = float(n) / prefix[symbol]
             return format % locals()
     return format % dict(symbol=symbols[0], value=n)
