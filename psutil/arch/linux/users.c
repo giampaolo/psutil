d30d41633123ec0cb6b4d8a9b2de0ab1242b6d47
Alberto Planas <aplanas@suse.com>
2023-08-22 15:54:09 +0200

    Use logind instead of utmp because of Y2038
    Bi-arch systems line x86-64 present the Y2038 problem, where an overflow
    can be produced because some glibc compatibility decissions (see
    https://github.com/thkukuk/utmpx/blob/main/Y2038.md for more
    information)
    
    This patch uses logind from systemd instead of utmp on Linux systems, if
    the systemd version is support the new API (>= 254).
    
    Signed-off-by: Alberto Planas <aplanas@suse.com>

--- psutil/arch/linux/users.c
+++ psutil/arch/linux/users.c
@@ -5,13 +5,134 @@
  */
 
 #include <Python.h>
+#include <dlfcn.h>
 #include <utmp.h>
 
 #include "../../_psutil_common.h"
 
+/* Systemd function signatures that will be loaded */
+int (*sd_booted)(void);
+int (*sd_get_sessions)(char ***);
+int (*sd_session_get_leader)(const char *, pid_t *);
+int (*sd_session_get_remote_host)(const char *,char **);
+int (*sd_session_get_start_time)(const char *, uint64_t *);
+int (*sd_session_get_tty)(const char *, char **);
+int (*sd_session_get_username)(const char *, char **);
 
-PyObject *
-psutil_users(PyObject *self, PyObject *args) {
+#define dlsym_check(__h, __fn) do {           \
+    __fn = dlsym(__h, #__fn);                 \
+    if (dlerror() != NULL || __fn == NULL) {  \
+        dlclose(__h);                         \
+        return NULL;                          \
+    }                                         \
+} while (0)
+
+static void *
+load_systemd() {
+    void *handle = dlopen("libsystemd.so.0", RTLD_LAZY);
+    if (dlerror() != NULL || handle == NULL)
+        return NULL;
+
+    dlsym_check(handle, sd_booted);
+    dlsym_check(handle, sd_get_sessions);
+    dlsym_check(handle, sd_session_get_leader);
+    dlsym_check(handle, sd_session_get_remote_host);
+    dlsym_check(handle, sd_session_get_start_time);
+    dlsym_check(handle, sd_session_get_tty);
+    dlsym_check(handle, sd_session_get_username);
+
+    return handle;
+}
+
+static PyObject *
+psutil_users_systemd(PyObject *self, PyObject *args) {
+    char **sessions_list = NULL;
+    PyObject *py_retlist = PyList_New(0);
+    PyObject *py_tuple = NULL;
+    PyObject *py_username = NULL;
+    PyObject *py_tty = NULL;
+    PyObject *py_hostname = NULL;
+    PyObject *py_user_proc = NULL;
+    double tstamp = 0.0;
+    pid_t pid = 0;
+
+    if (py_retlist == NULL)
+        return NULL;
+    int sessions = sd_get_sessions(&sessions_list);
+    for (int i = 0; i < sessions; i++) {
+        const char *session_id = sessions_list[i];
+        py_tuple = NULL;
+        py_user_proc = NULL;
+        py_user_proc = Py_True;
+
+        char *username = NULL;
+        if (sd_session_get_username(session_id, &username) < 0)
+            goto error;
+        py_username = PyUnicode_DecodeFSDefault(username);
+        free(username);
+        if (! py_username)
+            goto error;
+
+        char *tty = NULL;
+        if (sd_session_get_tty(session_id, &tty) < 0) {
+            py_tty = PyUnicode_DecodeFSDefault("n/a");
+        } else {
+            py_tty = PyUnicode_DecodeFSDefault(tty);
+            free(tty);
+        }
+        if (! py_tty)
+            goto error;
+
+        char *hostname = NULL;
+        if (sd_session_get_remote_host(session_id, &hostname) < 0)
+            goto error;
+        py_hostname = PyUnicode_DecodeFSDefault(hostname);
+        free(hostname);
+        if (! py_hostname)
+            goto error;
+
+        uint64_t usec = 0;
+        if (sd_session_get_start_time(session_id, &usec) < 0)
+           goto error;
+        tstamp = (double)usec / 1000000.0;
+
+        if (sd_session_get_leader(session_id, &pid) < 0)
+           goto error;
+
+        py_tuple = Py_BuildValue(
+            "OOOdO" _Py_PARSE_PID,
+            py_username,              // username
+            py_tty,                   // tty
+            py_hostname,              // hostname
+            tstamp,                   // tstamp
+            py_user_proc,             // (bool) user process
+            pid                       // process id
+        );
+        if (! py_tuple)
+            goto error;
+        if (PyList_Append(py_retlist, py_tuple))
+            goto error;
+        Py_CLEAR(py_username);
+        Py_CLEAR(py_tty);
+        Py_CLEAR(py_hostname);
+        Py_CLEAR(py_tuple);
+        free (sessions_list[i]);
+    }
+    free(sessions_list);
+    return py_retlist;
+
+error:
+    Py_XDECREF(py_username);
+    Py_XDECREF(py_tty);
+    Py_XDECREF(py_hostname);
+    Py_XDECREF(py_tuple);
+    Py_DECREF(py_retlist);
+    free(sessions_list);
+    return NULL;
+}
+
+static PyObject *
+psutil_users_utmp(PyObject *self, PyObject *args) {
     struct utmp *ut;
     PyObject *py_retlist = PyList_New(0);
     PyObject *py_tuple = NULL;
@@ -69,4 +190,13 @@ error:
     Py_DECREF(py_retlist);
     endutent();
     return NULL;
+}
+
+PyObject *
+psutil_users(PyObject *self, PyObject *args) {
+  void *handle = load_systemd();
+  if (handle && sd_booted())
+      return psutil_users_systemd(self, args);
+  else
+    return psutil_users_utmp(self, args);
 }
