99d35d75b150b4e3863606a2b90f42586138960c
Giampaolo Rodola <g.rodola@gmail.com>
2024-12-20 21:59:13 +0100

    use str.format()

--- scripts/free.py
+++ scripts/free.py
@@ -18,9 +18,11 @@ import psutil
 def main():
     virt = psutil.virtual_memory()
     swap = psutil.swap_memory()
-    templ = "%-7s %10s %10s %10s %10s %10s %10s"
-    print(templ % ('', 'total', 'used', 'free', 'shared', 'buffers', 'cache'))
-    sect = templ % (
+    templ = "{:<7} {:>10} {:>10} {:>10} {:>10} {:>10} {:>10}"
+    print(
+        templ.format('', 'total', 'used', 'free', 'shared', 'buffers', 'cache')
+    )
+    sect = templ.format(
         'Mem:',
         int(virt.total / 1024),
         int(virt.used / 1024),
@@ -30,7 +32,7 @@ def main():
         int(getattr(virt, 'cached', 0) / 1024),
     )
     print(sect)
-    sect = templ % (
+    sect = templ.format(
         'Swap:',
         int(swap.total / 1024),
         int(swap.used / 1024),
