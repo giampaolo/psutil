cfbda3250de28f7eb34403fa1bb0c31f7ae8e262
Giampaolo Rodola <g.rodola@gmail.com>
2024-12-21 13:40:40 +0100

    address _pswindows.py and _pssunos.py

--- psutil/_pssunos.py
+++ psutil/_pssunos.py
@@ -352,20 +352,20 @@ def wrap_exceptions(fun):
     def wrapper(self, *args, **kwargs):
         try:
             return fun(self, *args, **kwargs)
-        except (FileNotFoundError, ProcessLookupError):
+        except (FileNotFoundError, ProcessLookupError) as err:
             # ENOENT (no such file or directory) gets raised on open().
             # ESRCH (no such process) can get raised on read() if
             # process is gone in meantime.
             if not pid_exists(self.pid):
-                raise NoSuchProcess(self.pid, self._name)
+                raise NoSuchProcess(self.pid, self._name) from err
             else:
-                raise ZombieProcess(self.pid, self._name, self._ppid)
-        except PermissionError:
-            raise AccessDenied(self.pid, self._name)
-        except OSError:
+                raise ZombieProcess(self.pid, self._name, self._ppid) from err
+        except PermissionError as err:
+            raise AccessDenied(self.pid, self._name) from err
+        except OSError as err:
             if self.pid == 0:
                 if 0 in pids():
-                    raise AccessDenied(self.pid, self._name)
+                    raise AccessDenied(self.pid, self._name) from err
                 else:
                     raise
             raise
