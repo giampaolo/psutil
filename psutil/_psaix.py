ee0c5f9a3496d591c893c6ddfc086dc8a6afc912
Giampaolo Rodola <g.rodola@gmail.com>
2024-12-21 13:36:09 +0100

    port _psaix.py

--- psutil/_psaix.py
+++ psutil/_psaix.py
@@ -319,16 +319,16 @@ def wrap_exceptions(fun):
     def wrapper(self, *args, **kwargs):
         try:
             return fun(self, *args, **kwargs)
-        except (FileNotFoundError, ProcessLookupError):
+        except (FileNotFoundError, ProcessLookupError) as err:
             # ENOENT (no such file or directory) gets raised on open().
             # ESRCH (no such process) can get raised on read() if
             # process is gone in meantime.
             if not pid_exists(self.pid):
-                raise NoSuchProcess(self.pid, self._name)
+                raise NoSuchProcess(self.pid, self._name) from err
             else:
-                raise ZombieProcess(self.pid, self._name, self._ppid)
-        except PermissionError:
-            raise AccessDenied(self.pid, self._name)
+                raise ZombieProcess(self.pid, self._name, self._ppid) from err
+        except PermissionError as err:
+            raise AccessDenied(self.pid, self._name) from err
 
     return wrapper
 
