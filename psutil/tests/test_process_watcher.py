7cb3517f3bfd7adb1bd0a8e63f2c6b39b709b0ef
Giampaolo Rodola <g.rodola@gmail.com>
2025-02-26 01:41:57 +0100

    add test case

--- psutil/tests/test_process_watcher.py
+++ psutil/tests/test_process_watcher.py
@@ -6,6 +6,9 @@
 
 """Tests for psutil.ProcessWatcher class."""
 
+import os
+import time
+
 import psutil
 from psutil.tests import PsutilTestCase
 
@@ -17,6 +20,31 @@ class TestProcessWatcher(PsutilTestCase):
     def tearDown(self):
         self.pw.close()
 
-    def test_it(self):
-        pass
-        # proc = self.spawn_testproc()
+    def read_until_pid(self, pid, timeout=2):
+        stop_at = time.monotonic() + timeout
+        while time.monotonic() < stop_at:
+            event = self.pw.read()
+            if event["pid"] == pid:
+                return event
+        raise TimeoutError("timed out")
+
+    def test_fork_then_exit(self):
+        proc = self.spawn_testproc()
+        event = self.read_until_pid(proc.pid)
+        assert event == {
+            "pid": proc.pid,
+            "event": psutil.PROC_EVENT_FORK,
+            "parent_pid": os.getpid(),
+        }
+
+        event = self.read_until_pid(proc.pid)
+        assert event == {"pid": proc.pid, "event": psutil.PROC_EVENT_EXEC}
+
+        proc.terminate()
+        proc.wait()
+        event = self.read_until_pid(proc.pid)
+        assert event == {
+            "pid": proc.pid,
+            "event": psutil.PROC_EVENT_EXIT,
+            "exit_code": abs(proc.returncode),
+        }
