b70ed08c70438bf7277f72e3016184f482781adb
Giampaolo Rodola <g.rodola@gmail.com>
2025-03-06 00:16:39 +0100

    forkstat.py: give different colors to different events

--- scripts/forkstat.py
+++ scripts/forkstat.py
@@ -13,13 +13,33 @@ import sys
 import time
 
 import psutil
+from psutil._common import hilite
 
 
 if not psutil.LINUX:
     sys.exit("linux only")
 
 procs_data = collections.defaultdict(dict)
-templ = "{:<8} {:>8} {:>8} {:>8} {:>8} {:>5}"
+templ = "{}  {:>8} {:>8} {:>8} {:>8} {:>5}"
+
+
+def colored_event(ev):
+    shortname = str(ev._name_).split("_")[-1].lower()
+
+    if not psutil.LINUX:
+        return shortname
+    color_map = {
+        psutil.PROC_EVENT_FORK: "green",
+        psutil.PROC_EVENT_EXIT: "red",
+        psutil.PROC_EVENT_COMM: "blue",
+        psutil.PROC_EVENT_COREDUMP: "brown",
+        psutil.PROC_EVENT_EXEC: "yellow",
+        psutil.PROC_EVENT_GID: "grey",
+        psutil.PROC_EVENT_PTRACE: "lightblue",
+        psutil.PROC_EVENT_SID: "violet",
+        psutil.PROC_EVENT_UID: "darkgrey",
+    }
+    return hilite(shortname, color_map.get(ev))
 
 
 def callback(d):
@@ -48,12 +68,11 @@ def callback(d):
             d["elapsed"] = f"{elapsed:.3f}s"
             del procs_data[pid]
 
-    d["event"] = str(d["event"]._name_).split("_")[-1].lower()
     info = "thread" if d.get("is_thread") else ""
     parent = d.get("parent_pid", "")
     print(
         templ.format(
-            d["event"],
+            colored_event(d["event"]) + " ",
             d["pid"],
             info,
             parent,
