c78258d6315f10197dc959be23325b9fa2208969
Giampaolo Rodola <g.rodola@gmail.com>
2025-03-05 01:52:03 +0100

    rm INITERR from osx

--- psutil/_psutil_osx.c
+++ psutil/_psutil_osx.c
@@ -21,8 +21,6 @@
 #include "arch/osx/sys.h"
 
 
-#define INITERR return NULL
-
 static PyMethodDef mod_methods[] = {
     // --- per-process functions
     {"proc_cmdline", psutil_proc_cmdline, METH_VARARGS},
@@ -81,56 +79,54 @@ PyObject *
 PyInit__psutil_osx(void) {
     PyObject *mod = PyModule_Create(&moduledef);
     if (mod == NULL)
-        INITERR;
+        return NULL;
 
 #ifdef Py_GIL_DISABLED
     PyUnstable_Module_SetGIL(mod, Py_MOD_GIL_NOT_USED);
 #endif
 
     if (psutil_setup() != 0)
-        INITERR;
+        return NULL;
 
     if (PyModule_AddIntConstant(mod, "version", PSUTIL_VERSION))
-        INITERR;
+        return NULL;
     // process status constants, defined in:
     // http://fxr.watson.org/fxr/source/bsd/sys/proc.h?v=xnu-792.6.70#L149
     if (PyModule_AddIntConstant(mod, "SIDL", SIDL))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "SRUN", SRUN))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "SSLEEP", SSLEEP))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "SSTOP", SSTOP))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "SZOMB", SZOMB))
-        INITERR;
+        return NULL;
     // connection status constants
     if (PyModule_AddIntConstant(mod, "TCPS_CLOSED", TCPS_CLOSED))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_CLOSING", TCPS_CLOSING))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_CLOSE_WAIT", TCPS_CLOSE_WAIT))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_LISTEN", TCPS_LISTEN))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_ESTABLISHED", TCPS_ESTABLISHED))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_SYN_SENT", TCPS_SYN_SENT))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_SYN_RECEIVED", TCPS_SYN_RECEIVED))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_FIN_WAIT_1", TCPS_FIN_WAIT_1))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_FIN_WAIT_2", TCPS_FIN_WAIT_2))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_LAST_ACK", TCPS_LAST_ACK))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "TCPS_TIME_WAIT", TCPS_TIME_WAIT))
-        INITERR;
+        return NULL;
     if (PyModule_AddIntConstant(mod, "PSUTIL_CONN_NONE", PSUTIL_CONN_NONE))
-        INITERR;
+        return NULL;
 
-    if (mod == NULL)
-        INITERR;
     return mod;
 }
