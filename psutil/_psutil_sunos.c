cefe102f374a19b1084221db947948c1b1059a85
Giampaolo Rodola <g.rodola@gmail.com>
2025-04-28 05:50:42 +0000

    move disk funcs
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>

--- psutil/_psutil_sunos.c
+++ psutil/_psutil_sunos.c
@@ -28,8 +28,6 @@
 #include <sys/proc.h>
 #include <sys/swap.h>
 #include <sys/sysinfo.h>
-#include <sys/mntent.h>  // for MNTTAB
-#include <sys/mnttab.h>
 #include <sys/procfs.h>
 #include <sys/sockio.h>
 #include <sys/socket.h>
@@ -56,6 +54,7 @@
 #include "arch/all/init.h"
 #include "_psutil_posix.h"
 
+#include "arch/sunos/disk.h"
 #include "arch/sunos/mem.h"
 #include "arch/sunos/environ.h"
 
@@ -589,61 +588,6 @@ error:
 }
 
 
-/*
- * Return disk mounted partitions as a list of tuples including device,
- * mount point and filesystem type.
- */
-static PyObject *
-psutil_disk_partitions(PyObject *self, PyObject *args) {
-    FILE *file;
-    struct mnttab mt;
-    PyObject *py_dev = NULL;
-    PyObject *py_mountp = NULL;
-    PyObject *py_tuple = NULL;
-    PyObject *py_retlist = PyList_New(0);
-
-    if (py_retlist == NULL)
-        return NULL;
-
-    file = fopen(MNTTAB, "rb");
-    if (file == NULL) {
-        PyErr_SetFromErrno(PyExc_OSError);
-        goto error;
-    }
-
-    while (getmntent(file, &mt) == 0) {
-        py_dev = PyUnicode_DecodeFSDefault(mt.mnt_special);
-        if (! py_dev)
-            goto error;
-        py_mountp = PyUnicode_DecodeFSDefault(mt.mnt_mountp);
-        if (! py_mountp)
-            goto error;
-        py_tuple = Py_BuildValue(
-            "(OOss)",
-            py_dev,           // device
-            py_mountp,        // mount point
-            mt.mnt_fstype,    // fs type
-            mt.mnt_mntopts);  // options
-        if (py_tuple == NULL)
-            goto error;
-        if (PyList_Append(py_retlist, py_tuple))
-            goto error;
-        Py_CLEAR(py_dev);
-        Py_CLEAR(py_mountp);
-        Py_CLEAR(py_tuple);
-    }
-    fclose(file);
-    return py_retlist;
-
-error:
-    Py_XDECREF(py_dev);
-    Py_XDECREF(py_mountp);
-    Py_XDECREF(py_tuple);
-    Py_DECREF(py_retlist);
-    if (file != NULL)
-        fclose(file);
-    return NULL;
-}
 
 
 /*
@@ -697,62 +641,6 @@ error:
 }
 
 
-/*
- * Return disk IO statistics.
- */
-static PyObject *
-psutil_disk_io_counters(PyObject *self, PyObject *args) {
-    kstat_ctl_t *kc;
-    kstat_t *ksp;
-    kstat_io_t kio;
-    PyObject *py_retdict = PyDict_New();
-    PyObject *py_disk_info = NULL;
-
-    if (py_retdict == NULL)
-        return NULL;
-    kc = kstat_open();
-    if (kc == NULL) {
-        PyErr_SetFromErrno(PyExc_OSError);;
-        goto error;
-    }
-    ksp = kc->kc_chain;
-    while (ksp != NULL) {
-        if (ksp->ks_type == KSTAT_TYPE_IO) {
-            if (strcmp(ksp->ks_class, "disk") == 0) {
-                if (kstat_read(kc, ksp, &kio) == -1) {
-                    kstat_close(kc);
-                    return PyErr_SetFromErrno(PyExc_OSError);;
-                }
-                py_disk_info = Py_BuildValue(
-                    "(IIKKLL)",
-                    kio.reads,
-                    kio.writes,
-                    kio.nread,
-                    kio.nwritten,
-                    kio.rtime / 1000 / 1000,  // from nano to milli secs
-                    kio.wtime / 1000 / 1000   // from nano to milli secs
-                );
-                if (!py_disk_info)
-                    goto error;
-                if (PyDict_SetItemString(py_retdict, ksp->ks_name,
-                                         py_disk_info))
-                    goto error;
-                Py_CLEAR(py_disk_info);
-            }
-        }
-        ksp = ksp->ks_next;
-    }
-    kstat_close(kc);
-
-    return py_retdict;
-
-error:
-    Py_XDECREF(py_disk_info);
-    Py_DECREF(py_retdict);
-    if (kc != NULL)
-        kstat_close(kc);
-    return NULL;
-}
 
 
 /*
---
+++ psutil/arch/sunos/disk.c
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2009, Giampaolo Rodola'. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <Python.h>
+
+#include <sys/mntent.h>
+#include <sys/mnttab.h>
+#include <kstat.h>
+
+
+PyObject *
+psutil_disk_io_counters(PyObject *self, PyObject *args) {
+    kstat_ctl_t *kc;
+    kstat_t *ksp;
+    kstat_io_t kio;
+    PyObject *py_retdict = PyDict_New();
+    PyObject *py_disk_info = NULL;
+
+    if (py_retdict == NULL)
+        return NULL;
+    kc = kstat_open();
+    if (kc == NULL) {
+        PyErr_SetFromErrno(PyExc_OSError);;
+        goto error;
+    }
+    ksp = kc->kc_chain;
+    while (ksp != NULL) {
+        if (ksp->ks_type == KSTAT_TYPE_IO) {
+            if (strcmp(ksp->ks_class, "disk") == 0) {
+                if (kstat_read(kc, ksp, &kio) == -1) {
+                    kstat_close(kc);
+                    return PyErr_SetFromErrno(PyExc_OSError);;
+                }
+                py_disk_info = Py_BuildValue(
+                    "(IIKKLL)",
+                    kio.reads,
+                    kio.writes,
+                    kio.nread,
+                    kio.nwritten,
+                    kio.rtime / 1000 / 1000,  // from nano to milli secs
+                    kio.wtime / 1000 / 1000   // from nano to milli secs
+                );
+                if (!py_disk_info)
+                    goto error;
+                if (PyDict_SetItemString(py_retdict, ksp->ks_name,
+                                         py_disk_info))
+                    goto error;
+                Py_CLEAR(py_disk_info);
+            }
+        }
+        ksp = ksp->ks_next;
+    }
+    kstat_close(kc);
+
+    return py_retdict;
+
+error:
+    Py_XDECREF(py_disk_info);
+    Py_DECREF(py_retdict);
+    if (kc != NULL)
+        kstat_close(kc);
+    return NULL;
+}
+
+
+PyObject *
+psutil_disk_partitions(PyObject *self, PyObject *args) {
+    FILE *file;
+    struct mnttab mt;
+    PyObject *py_dev = NULL;
+    PyObject *py_mountp = NULL;
+    PyObject *py_tuple = NULL;
+    PyObject *py_retlist = PyList_New(0);
+
+    if (py_retlist == NULL)
+        return NULL;
+
+    file = fopen(MNTTAB, "rb");
+    if (file == NULL) {
+        PyErr_SetFromErrno(PyExc_OSError);
+        goto error;
+    }
+
+    while (getmntent(file, &mt) == 0) {
+        py_dev = PyUnicode_DecodeFSDefault(mt.mnt_special);
+        if (! py_dev)
+            goto error;
+        py_mountp = PyUnicode_DecodeFSDefault(mt.mnt_mountp);
+        if (! py_mountp)
+            goto error;
+        py_tuple = Py_BuildValue(
+            "(OOss)",
+            py_dev,           // device
+            py_mountp,        // mount point
+            mt.mnt_fstype,    // fs type
+            mt.mnt_mntopts);  // options
+        if (py_tuple == NULL)
+            goto error;
+        if (PyList_Append(py_retlist, py_tuple))
+            goto error;
+        Py_CLEAR(py_dev);
+        Py_CLEAR(py_mountp);
+        Py_CLEAR(py_tuple);
+    }
+    fclose(file);
+    return py_retlist;
+
+error:
+    Py_XDECREF(py_dev);
+    Py_XDECREF(py_mountp);
+    Py_XDECREF(py_tuple);
+    Py_DECREF(py_retlist);
+    if (file != NULL)
+        fclose(file);
+    return NULL;
+}
