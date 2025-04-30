giampaolo/psutil

commit de54f54a7c5790847ee8297c2254dde3395ed2aa

Handle alternate NT path forms
`convert_dos_path` does not currently handle UNC paths or paths of the form `\??\X:` (as returned by Wine).
This results in the `psutil/tests/test_process.py::TestProcess::test_exe` test failing in the following cases:
* Python is run from a network share; or
* The tests are performed under Wine on a drive other than the one Python is installed on.

Add handling for the following NT path forms:
* `\\server\share`
* `\Device\Mup\server\share` -> `\\server\share`
* `\??\UNC\server\share` -> `\\server\share`
* `\??\X:` -> `X:`

Signed-off-by: Ben Peddell <klightspeed@killerwolves.net>

psutil/_pswindows.py
@@ -184,12 +184,21 @@ pio = namedtuple('pio', ['read_count', 'write_count',
 @functools.lru_cache(maxsize=512)
 def convert_dos_path(s):
     r"""Convert paths using native DOS format like:
-        "\Device\HarddiskVolume1\Windows\systemew\file.txt"
+        "\Device\HarddiskVolume1\Windows\systemew\file.txt" or
+        "\??\C:\Windows\systemew\file.txt"
     into:
         "C:\Windows\systemew\file.txt".
     """
+    if s.startswith('\\\\'):
+        return s
     rawdrive = '\\'.join(s.split('\\')[:3])
-    driveletter = cext.QueryDosDevice(rawdrive)
+    if rawdrive in {"\\??\\UNC", "\\Device\\Mup"}:
+        rawdrive = '\\'.join(s.split('\\')[:4])
+        driveletter = "\\\\" + s.split('\\')[3]
+    elif rawdrive.startswith('\\??\\'):
+        driveletter = s.split('\\')[2]
+    else:
+        driveletter = cext.QueryDosDevice(rawdrive)
     remainder = s[len(rawdrive) :]
     return os.path.join(driveletter, remainder)
 

commit 9b19ab5487fd921da7921be0c413879db9faf099

Fix UNC path under Python 3.10 and older
Python 3.10 and older only treat full `\\host\share` UNC paths as being rooted at the path of that share, while Python 3.11 and newer also treat `\\host` UNC paths as being rooted at the path of the host.
Extract the `\\host\share` of an NT UNC path as the drive letter rather than only the `\\host` part of the path.

Fixes: de54f54 Handle alternate NT path forms
Signed-off-by: Ben Peddell <klightspeed@killerwolves.net>

psutil/_pswindows.py
@@ -193,8 +193,8 @@ def convert_dos_path(s):
         return s
     rawdrive = '\\'.join(s.split('\\')[:3])
     if rawdrive in {"\\??\\UNC", "\\Device\\Mup"}:
-        rawdrive = '\\'.join(s.split('\\')[:4])
-        driveletter = "\\\\" + s.split('\\')[3]
+        rawdrive = '\\'.join(s.split('\\')[:5])
+        driveletter = '\\\\' + '\\'.join(s.split('\\')[3:5])
     elif rawdrive.startswith('\\??\\'):
         driveletter = s.split('\\')[2]
     else: