15d18d3f9afa30c493475e8cf257f7bc74195d57
Giampaolo Rodola <g.rodola@gmail.com>
2025-08-13 17:35:37 +0200

    osx: port psutil_net_io_counters()

--- psutil/arch/osx/net.c
+++ psutil/arch/osx/net.c
@@ -29,42 +29,29 @@ psutil_net_io_counters(PyObject *self, PyObject *args) {
     mib[3] = 0;                // address family
     mib[4] = NET_RT_IFLIST2;   // operation
     mib[5] = 0;
-    size_t len;
     PyObject *py_ifc_info = NULL;
     PyObject *py_retdict = PyDict_New();
 
     if (py_retdict == NULL)
         return NULL;
 
-    if (sysctl(mib, 6, NULL, &len, NULL, 0) < 0) {
-        PyErr_SetFromErrno(PyExc_OSError);
-        goto error;
-    }
-
-    buf = malloc(len);
-    if (buf == NULL) {
-        PyErr_NoMemory();
-        goto error;
-    }
-
-    if (sysctl(mib, 6, buf, &len, NULL, 0) < 0) {
-        PyErr_SetFromErrno(PyExc_OSError);
-        goto error;
-    }
+    buf = psutil_sysctl_malloc(mib, 6, &len);  // <- use helper
+    if (buf == NULL)
+        return NULL;
 
     lim = buf + len;
 
     for (next = buf; next < lim; ) {
-        // Check we have enough space for if_msghdr
         if ((size_t)(lim - next) < sizeof(struct if_msghdr)) {
-            // buffer too small for header
-            psutil_debug("struct xfile size mismatch");
+            psutil_debug("struct if_msghdr size mismatch");
+            break;
         }
 
         ifm = (struct if_msghdr *)next;
 
         if (ifm->ifm_msglen == 0 || next + ifm->ifm_msglen > lim) {
             psutil_debug("ifm_msglen size mismatch");
+            break;
         }
 
         next += ifm->ifm_msglen;
@@ -75,17 +62,18 @@ psutil_net_io_counters(PyObject *self, PyObject *args) {
 
             if ((char *)if2m + sizeof(struct if_msghdr2) > lim) {
                 psutil_debug("if_msghdr2 + sockaddr_dl mismatch");
+                continue;
             }
 
             struct sockaddr_dl *sdl = (struct sockaddr_dl *)(if2m + 1);
 
             if ((char *)sdl + sizeof(struct sockaddr_dl) > lim) {
                 psutil_debug("not enough buffer for sockaddr_dl");
+                continue;
             }
 
             char ifc_name[32];
             size_t namelen = sdl->sdl_nlen;
-
             if (namelen >= sizeof(ifc_name))
                 namelen = sizeof(ifc_name) - 1;
 
