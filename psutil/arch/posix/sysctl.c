f36cfd0f871a28160abed52bdfba702ce3efa10e
Giampaolo Rodola <g.rodola@gmail.com>
2025-08-13 21:17:26 +0200

    avoid unnuecessary var type re-declaration

--- psutil/arch/posix/sysctl.c
+++ psutil/arch/posix/sysctl.c
@@ -57,7 +57,7 @@ psutil_sysctl_malloc(int *mib, u_int miblen, char **buf, size_t *buflen) {
             return -1;
         }
 
-        size_t len = needed;
+        len = needed;
         ret = sysctl(mib, miblen, buffer, &len, NULL, 0);
 
         if (ret == 0) {
