834a7e4313132369d205051592b122b2575415e6
Giampaolo Rodola <g.rodola@gmail.com>
2025-10-19 14:25:25 +0200

    Update HISTORY + doc

--- psutil/__init__.py
+++ psutil/__init__.py
@@ -205,7 +205,7 @@ if hasattr(_psplatform.Process, "rlimit"):
 AF_LINK = _psplatform.AF_LINK
 
 __author__ = "Giampaolo Rodola'"
-__version__ = "7.1.0"
+__version__ = "7.1.1"
 version_info = tuple(int(num) for num in __version__.split('.'))
 
 _timer = getattr(time, 'monotonic', time.time)
