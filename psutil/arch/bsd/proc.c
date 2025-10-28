71056cd7caed73a85db795098602b31df4812a07
Giampaolo Rodola <g.rodola@gmail.com>
2025-10-28 16:25:05 +0100

    OpenBSD: right status for zombie is SDEAD, not SZOMB

--- psutil/arch/bsd/proc.c
+++ psutil/arch/bsd/proc.c
@@ -124,8 +124,13 @@ is_zombie(size_t pid) {
         return 0;
     }
 
-#ifdef PSUTIL_FREEBSD
+#if defined(PSUTIL_FREEBSD)
     return kp.ki_stat == SZOMB;
+#elif defined(PSUTIL_OPENBSD)
+    // According to /usr/include/sys/proc.h SZOMB is unused.
+    // test_zombie_process() shows that SDEAD is the right
+    // equivalent.
+    return ((kp.p_stat == SZOMB) || (kp.p_stat == SDEAD));
 #else
     return kp.p_stat == SZOMB;
 #endif
