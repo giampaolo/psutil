5aaafdd42167e12cc95ffe847c54ce299b16cb60
Giampaolo Rodola <g.rodola@gmail.com>
2025-11-06 04:05:50 +0100

    Small refact

--- psutil/arch/osx/memalloc.c
+++ psutil/arch/osx/memalloc.c
@@ -25,16 +25,16 @@ PyObject *
 psutil_malloc_info(PyObject *self, PyObject *args) {
     malloc_statistics_t stats = {0};
     malloc_zone_t *zone = malloc_default_zone();
+    uint64_t heap_used, mmap_used, heap_total;
 
     if (!zone)
         return psutil_runtime_error("malloc_default_zone() failed");
 
     malloc_zone_statistics(zone, &stats);
 
-    uint64_t heap_used = (uint64_t)stats.size_in_use;
-    uint64_t mmap_used = 0;
-    uint64_t heap_total = (uint64_t)stats.size_allocated;
-
+    heap_used = (uint64_t)stats.size_in_use;
+    mmap_used = 0;
+    heap_total = (uint64_t)stats.size_allocated;
     return Py_BuildValue("KKK", heap_used, mmap_used, heap_total);
 }
 
