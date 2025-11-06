3dcce865711390490cb82db7458c4c713b0c22e6
Giampaolo Rodola <g.rodola@gmail.com>
2025-11-07 00:19:51 +0100

    Include heap_used and mmap_used

--- psutil/test/memleak.py
+++ psutil/test/memleak.py
@@ -104,12 +104,15 @@ class MemoryLeakTestCase(unittest.TestCase):
 
     def _get_mem(self):
         mem = thisproc.memory_full_info()
-        malloc = 0
+        heap_used = mmap_used = 0
         if hasattr(psutil._psplatform, "malloc_info"):
-            # Linux, macOS
-            malloc = psutil._psplatform.malloc_info().heap_used
+            # Linux, macOS, BSD
+            mallinfo = psutil._psplatform.malloc_info()
+            heap_used = mallinfo.heap_used
+            mmap_used = mallinfo.mmap_used
         return {
-            "malloc": malloc,
+            "heap": heap_used,
+            "mmap": mmap_used,
             "uss": getattr(mem, "uss", 0),
             "rss": mem.rss,
             "vms": mem.vms,
