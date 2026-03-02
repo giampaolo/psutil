d318efc3bbe00e9d9b731b01a3ae61129e084e01
Giampaolo Rodola <g.rodola@gmail.com>
2026-03-02 14:37:37 +0100

    Rename psutil_proc_oneshot_pidtaskinfo()

--- psutil/arch/osx/proc.c
+++ psutil/arch/osx/proc.c
@@ -120,7 +120,7 @@ psutil_proc_oneshot_kinfo(PyObject *self, PyObject *args) {
  * processes.
  */
 PyObject *
-psutil_proc_pidtaskinfo_oneshot(PyObject *self, PyObject *args) {
+psutil_proc_oneshot_pidtaskinfo(PyObject *self, PyObject *args) {
     pid_t pid;
     struct proc_taskinfo pti;
     unsigned long maj_faults;
