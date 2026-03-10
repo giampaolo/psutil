a6be074307f96b16999d82970491bfb965047720
Giampaolo Rodola <g.rodola@gmail.com>
2026-03-10 11:04:57 +0100

    Add auto-build.sh script

---
+++ docs/auto-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script uses inotifywait to watch for changes in the current directory
+# and its parents, and automatically rebuilds the Sphinx doc whenever a change
+# is detected. Requires: sudo apt install inotify-tools.
+
+DIR="."
+while inotifywait -r -e modify,create,delete,move "$DIR"; do
+    make html
+done
