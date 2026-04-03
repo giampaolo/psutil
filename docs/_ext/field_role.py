6da11daf87178275d1a390101206153fe589d228
Giampaolo Rodola <g.rodola@gmail.com>
2026-04-04 00:34:13 +0200

    Add docs/_ext/field_role.py

---
+++ docs/_ext/field_role.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2009, Giampaolo Rodola. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Sphinx extension providing the :field:`name` role for marking named
+tuple fields in the API doc.
+"""
+
+from docutils import nodes
+
+
+def field_role(
+    name, rawtext, text, lineno, inliner, options=None, content=None
+):
+    """Render :field:`name` as inline code (monospace bold)."""
+    node = nodes.literal(rawtext, text, classes=["field"])
+    return [node], []
+
+
+def setup(app):
+    app.add_role("field", field_role)
