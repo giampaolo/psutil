2eb2fa610fce1d54d9d63714dffdee081e9bcfa2
Santhosh Raju <santhosh.raju@gmail.com>
2026-04-07 07:43:34 +0200

    Remove kvm dependency for intrs.

--- psutil/arch/netbsd/cpu.c
+++ psutil/arch/netbsd/cpu.c
@@ -6,10 +6,6 @@
  */
 
 #include <Python.h>
-#include <fcntl.h>
-#include <kvm.h>
-#include <limits.h>
-#include <nlist.h>
 #include <sys/sched.h>
 #include <sys/sysctl.h>
 #include <uvm/uvm_extern.h>
@@ -28,44 +24,17 @@ PyObject *
 psutil_cpu_stats(PyObject *self, PyObject *args) {
     struct uvmexp_sysctl uv;
     int uvmexp_mib[] = {CTL_VM, VM_UVMEXP2};
-    const int cpu_count_nintr = 3;
-    char errbuf[_POSIX2_LINE_MAX];
 
+    // https://man.netbsd.org/man9/uvm.9
     if (psutil_sysctl(uvmexp_mib, 2, &uv, sizeof(uv)) != 0)
         return NULL;
-
-    kvm_t *kd = kvm_open(NULL, NULL, NULL, O_RDONLY, errbuf);
-    if (!kd) {
-        fprintf(stderr, "kvm_open: %s\n", errbuf);
-        return NULL;
-    }
-
-    struct nlist nl[] = {{.n_name = "_cpu_counts"}, {.n_name = NULL}};
-
-    if (kvm_nlist(kd, nl) != 0 || nl[0].n_value == 0) {
-        fprintf(stderr, "kvm_nlist(_cpu_counts): %s\n", kvm_geterr(kd));
-        kvm_close(kd);
-        return NULL;
-    }
-
-    /* Read cpu_counts[CPU_COUNT_NINTR] — a single int64_t */
-    uintptr_t addr = nl[0].n_value + cpu_count_nintr * sizeof(int64_t);
-    int64_t nintr = 0;
-
-    if (kvm_read(kd, addr, &nintr, sizeof(nintr)) != sizeof(nintr)) {
-        fprintf(stderr, "kvm_read(cpu_counts[NINTR]): %s\n", kvm_geterr(kd));
-        kvm_close(kd);
-        return NULL;
-    }
-
-    kvm_close(kd);
 
     return Py_BuildValue(
         "KKKKKKK",
         (uint64_t)uv.swtch,  // ctx switches
-        (uint64_t)nintr,  // interrupts
+        (uint64_t)uv.intrs,  // interrupts
         (uint64_t)uv.softs,  // soft interrupts
-        (uint64_t)uv.syscalls,  // syscalls - XXX always 0
+        (uint64_t)uv.syscalls,  // syscalls
         (uint64_t)uv.traps,  // traps
         (uint64_t)uv.faults,  // faults
         (uint64_t)uv.forks  // forks
